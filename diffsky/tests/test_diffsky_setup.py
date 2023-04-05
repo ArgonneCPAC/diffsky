3366933cbbac73579d9af9bd4bf235b7019d1a2d
Andrew Hearin <ahearin@anl.gov>
2023-04-05 09:54:24 -0500

    Add unit test of main branch DSPS

--- diffsky/tests/test_diffsky_setup.py
+++ diffsky/tests/test_diffsky_setup.py
@@ -2,5 +2,11 @@
 """
 
 
-def test_diffsky_basic_setup():
-    pass
+def test_diffsky_has_main_branch_dsps():
+    try:
+        from dsps.experimental import diffburst  # noqa
+
+        HAS_DSPS_MAIN = True
+    except ImportError:
+        HAS_DSPS_MAIN = False
+    assert HAS_DSPS_MAIN, "Failed to import dsps.experimental.diffburst"
