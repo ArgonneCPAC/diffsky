f9dbce99cb9cd8b5ce40f86ec468a6ecca61e98f
Andrew Hearin <ahearin@anl.gov>
2023-04-25 07:18:51 -0500

    Working kernel for photpop

--- diffsky/experimental/photpop.py
+++ diffsky/experimental/photpop.py
@@ -2,13 +2,24 @@
 """
 from dsps.sed import calc_ssp_weights_sfh_table_lognormal_mdf
 from jax import vmap, jit as jjit
-from dsps.cosmology import age_at_z
+from dsps.cosmology.flat_wcdm import _age_at_z_kern
 from jax import numpy as jnp
+from dsps.metallicity.mzr import mzr_model, DEFAULT_MZR_PDICT
+from dsps.constants import SFR_MIN
+from dsps.sed.stellar_age_weights import _calc_logsm_table_from_sfh_table
 
 
-_a = (None, 0, 0, None, None, None, 0)
+DEFAULT_MZR_PARAMS = jnp.array(list(DEFAULT_MZR_PDICT.values()))
+_linterp_vmap = jjit(vmap(jnp.interp, in_axes=(None, None, 0)))
+
+_g = (None, 0, 0, None, None, None, None)
 calc_ssp_weights_sfh_table_lognormal_mdf_vmap = jjit(
-    vmap(calc_ssp_weights_sfh_table_lognormal_mdf, in_axes=_a)
+    vmap(calc_ssp_weights_sfh_table_lognormal_mdf, in_axes=_g)
+)
+
+_b = (None, 0, None)
+_calc_logsm_table_from_sfh_table_vmap = jjit(
+    vmap(_calc_logsm_table_from_sfh_table, in_axes=_b)
 )
 
 
@@ -21,6 +32,32 @@ def get_obs_photometry_singlez(
     gal_sfr_table,
     cosmo_params,
     z_obs,
+    met_params=DEFAULT_MZR_PARAMS,
     lgmet_scatter=0.2,
 ):
-    pass
+    t_obs = _age_at_z_kern(z_obs, *cosmo_params)
+    lgt_obs = jnp.log10(t_obs)
+    lgt_table = jnp.log10(gal_t_table)
+
+    gal_sfr_table = jnp.where(gal_sfr_table < SFR_MIN, SFR_MIN, gal_sfr_table)
+    gal_logsm_table = _calc_logsm_table_from_sfh_table_vmap(
+        gal_t_table, gal_sfr_table, SFR_MIN
+    )
+    gal_logsfr_table = jnp.log10(gal_sfr_table)
+
+    gal_logsm_t_obs = _linterp_vmap(lgt_obs, lgt_table, gal_logsm_table)
+    gal_logsfr_t_obs = _linterp_vmap(lgt_obs, lgt_table, gal_logsfr_table)
+    gal_logssfr_t_obs = gal_logsfr_t_obs - gal_logsm_t_obs
+
+    gal_lgmet = mzr_model(gal_logsm_t_obs, t_obs, *met_params[:-1])
+
+    smooth_ssp_weights = calc_ssp_weights_sfh_table_lognormal_mdf_vmap(
+        gal_t_table,
+        gal_sfr_table,
+        gal_lgmet,
+        lgmet_scatter,
+        ssp_lgmet,
+        ssp_lg_age,
+        t_obs,
+    )
+    return smooth_ssp_weights
---
+++ diffsky/experimental/tests/test_photpop.py
@@ -0,0 +1,35 @@
+"""
+"""
+import numpy as np
+from ..photpop import get_obs_photometry_singlez
+from dsps.cosmology import DEFAULT_COSMOLOGY
+
+
+def test_photpop_evaluates():
+    n_met, n_age = 20, 50
+    ssp_lgmet = np.linspace(-4, 0, n_met)
+    ssp_lg_age = np.linspace(5.5, 10.5, n_age) - 9
+
+    n_t = 100
+    gal_t_table = np.linspace(0.05, 13.8, n_t)
+
+    n_filters = 3
+    ssp_obsmag_table = np.random.uniform(0, 1, size=(n_met, n_age, n_filters))
+
+    n_gals = 50
+    gal_sfr_table = np.random.uniform(0, 1, size=(n_gals, n_t))
+
+    z_obs = 0.5
+
+    res = get_obs_photometry_singlez(
+        ssp_obsmag_table,
+        ssp_lgmet,
+        ssp_lg_age,
+        gal_t_table,
+        gal_sfr_table,
+        DEFAULT_COSMOLOGY,
+        z_obs,
+    )
+    weights, lgmet_weights, smooth_age_weights = res
+    assert weights.shape == (n_gals, n_met, n_age)
+    assert np.all(np.isfinite(weights))
