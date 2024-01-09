739df973458920da5d6e1d63f62d0d37c14b7001
Andrew Hearin <ahearin@anl.gov>
2024-01-09 12:59:52 -0600

    Bring in burstpop from tasso

---
+++ diffsky/burstpop/__init__.py
@@ -0,0 +1,9 @@
+"""
+"""
+# flake8: noqa
+
+from .diffburstpop import (
+    DEFAULT_DIFFBURSTPOP_PARAMS,
+    DEFAULT_DIFFBURSTPOP_U_PARAMS,
+    calc_bursty_age_weights_from_diffburstpop_params,
+)
