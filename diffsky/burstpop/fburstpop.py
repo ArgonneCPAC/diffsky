c06c5c3ebe97e1eba7a3130fb2df2d6851181c97
Andrew Hearin <ahearin@anl.gov>
2024-01-09 11:11:57 -0600

    Bring in burstpop from tasso

---
+++ diffsky/burstpop/fburstpop.py
@@ -0,0 +1,138 @@
+"""
+"""
+from collections import OrderedDict, namedtuple
+
+from jax import jit as jjit
+from jax import numpy as jnp
+from jax import vmap
+
+from ..utils import _inverse_sigmoid, _sigmoid
+
+LGSM_K = 5.0
+LGSSFR_K = 5.0
+
+DEFAULT_FBURSTPOP_PDICT = OrderedDict(
+    lgfburst_logsm_x0_x0=10.0,
+    lgfburst_logsm_x0_q=10.5,
+    lgfburst_logsm_x0_ms=9.5,
+    lgfburst_logsm_ylo_x0=-10.25,
+    lgfburst_logsm_ylo_q=-3.0,
+    lgfburst_logsm_ylo_ms=-2.0,
+    lgfburst_logsm_yhi_x0=-11.25,
+    lgfburst_logsm_yhi_q=-4.0,
+    lgfburst_logsm_yhi_ms=-2.5,
+)
+
+_LGSM_X0_BOUNDS = (8.0, 12.0)
+_LGSSFR_X0_BOUNDS = (-13.0, -7.0)
+_LGFBURST_BOUNDS = (-5.0, -1.0)
+FBURSTPOP_BOUNDS_PDICT = OrderedDict(
+    lgfburst_logsm_x0_x0=_LGSM_X0_BOUNDS,
+    lgfburst_logsm_x0_q=_LGSM_X0_BOUNDS,
+    lgfburst_logsm_x0_ms=_LGSM_X0_BOUNDS,
+    lgfburst_logsm_ylo_x0=_LGSSFR_X0_BOUNDS,
+    lgfburst_logsm_ylo_q=_LGFBURST_BOUNDS,
+    lgfburst_logsm_ylo_ms=_LGFBURST_BOUNDS,
+    lgfburst_logsm_yhi_x0=_LGSSFR_X0_BOUNDS,
+    lgfburst_logsm_yhi_q=_LGFBURST_BOUNDS,
+    lgfburst_logsm_yhi_ms=_LGFBURST_BOUNDS,
+)
+
+FburstPopParams = namedtuple("FburstPopParams", DEFAULT_FBURSTPOP_PDICT.keys())
+_FBURSTPOP_UPNAMES = ["u_" + key for key in DEFAULT_FBURSTPOP_PDICT.keys()]
+FburstPopUParams = namedtuple("FburstPopUParams", _FBURSTPOP_UPNAMES)
+
+
+DEFAULT_FBURSTPOP_PARAMS = FburstPopParams(**DEFAULT_FBURSTPOP_PDICT)
+FBURSTPOP_PBOUNDS = FburstPopParams(**FBURSTPOP_BOUNDS_PDICT)
+
+
+@jjit
+def get_lgfburst_from_fburstpop_u_params(fburstpop_u_params, logsm, logssfr):
+    fburstpop_params = get_bounded_fburstpop_params(fburstpop_u_params)
+    lgfburst = get_lgfburst_from_fburstpop_params(fburstpop_params, logsm, logssfr)
+    return lgfburst
+
+
+@jjit
+def get_lgfburst_from_fburstpop_params(fburstpop_params, logsm, logssfr):
+    lgfburst_logssfr_x0 = _sigmoid(
+        logsm,
+        fburstpop_params.lgfburst_logsm_x0_x0,
+        LGSM_K,
+        fburstpop_params.lgfburst_logsm_ylo_x0,
+        fburstpop_params.lgfburst_logsm_yhi_x0,
+    )
+    lgfburst_logssfr_q = _sigmoid(
+        logsm,
+        fburstpop_params.lgfburst_logsm_x0_q,
+        LGSM_K,
+        fburstpop_params.lgfburst_logsm_ylo_q,
+        fburstpop_params.lgfburst_logsm_yhi_q,
+    )
+    lgfburst_logssfr_ms = _sigmoid(
+        logsm,
+        fburstpop_params.lgfburst_logsm_x0_ms,
+        LGSSFR_K,
+        fburstpop_params.lgfburst_logsm_ylo_ms,
+        fburstpop_params.lgfburst_logsm_yhi_ms,
+    )
+
+    lgfburst = _sigmoid(
+        logssfr,
+        lgfburst_logssfr_x0,
+        LGSSFR_K,
+        lgfburst_logssfr_q,
+        lgfburst_logssfr_ms,
+    )
+    return lgfburst
+
+
+@jjit
+def _get_bounded_fburstpop_param(u_param, bound):
+    lo, hi = bound
+    mid = 0.5 * (lo + hi)
+    return _sigmoid(u_param, mid, 0.1, lo, hi)
+
+
+@jjit
+def _get_unbounded_fburstpop_param(param, bound):
+    lo, hi = bound
+    mid = 0.5 * (lo + hi)
+    return _inverse_sigmoid(param, mid, 0.1, lo, hi)
+
+
+_C = (0, 0)
+_get_bounded_fburstpop_params_kern = jjit(
+    vmap(_get_bounded_fburstpop_param, in_axes=_C)
+)
+_get_unbounded_fburstpop_params_kern = jjit(
+    vmap(_get_unbounded_fburstpop_param, in_axes=_C)
+)
+
+
+@jjit
+def get_bounded_fburstpop_params(u_params):
+    u_params = jnp.array([getattr(u_params, u_pname) for u_pname in _FBURSTPOP_UPNAMES])
+    params = _get_bounded_fburstpop_params_kern(
+        jnp.array(u_params), jnp.array(FBURSTPOP_PBOUNDS)
+    )
+    fburstpop_params = FburstPopParams(*params)
+    return fburstpop_params
+
+
+@jjit
+def get_unbounded_fburstpop_params(params):
+    params = jnp.array(
+        [getattr(params, pname) for pname in DEFAULT_FBURSTPOP_PARAMS._fields]
+    )
+    u_params = _get_unbounded_fburstpop_params_kern(
+        jnp.array(params), jnp.array(FBURSTPOP_PBOUNDS)
+    )
+    fburstpop_u_params = FburstPopUParams(*u_params)
+    return fburstpop_u_params
+
+
+DEFAULT_FBURSTPOP_U_PARAMS = FburstPopUParams(
+    *get_unbounded_fburstpop_params(DEFAULT_FBURSTPOP_PARAMS)
+)
---
+++ diffsky/burstpop/tests/test_tburstpop.py
@@ -0,0 +1,268 @@
+"""
+"""
+import numpy as np
+from jax import random as jran
+
+from .. import tburstpop as tbp
+from ..diffburst import (
+    _pureburst_age_weights_from_params,
+    _pureburst_age_weights_from_u_params,
+)
+
+TOL = 1e-2
+
+
+def test_param_u_param_names_propagate_properly():
+    gen = zip(
+        tbp.DEFAULT_TBURSTPOP_U_PARAMS._fields, tbp.DEFAULT_TBURSTPOP_PARAMS._fields
+    )
+    for u_key, key in gen:
+        assert u_key[:2] == "u_"
+        assert u_key[2:] == key
+
+    inferred_default_params = tbp.get_bounded_tburstpop_params(
+        tbp.DEFAULT_TBURSTPOP_U_PARAMS
+    )
+    assert set(inferred_default_params._fields) == set(
+        tbp.DEFAULT_TBURSTPOP_PARAMS._fields
+    )
+
+    inferred_default_u_params = tbp.get_unbounded_tburstpop_params(
+        tbp.DEFAULT_TBURSTPOP_PARAMS
+    )
+    assert set(inferred_default_u_params._fields) == set(
+        tbp.DEFAULT_TBURSTPOP_U_PARAMS._fields
+    )
+
+
+def test_get_bounded_tburstpop_params_fails_when_passing_params():
+    try:
+        tbp.get_bounded_tburstpop_params(tbp.DEFAULT_TBURSTPOP_PARAMS)
+        raise NameError("get_bounded_tburstpop_params should not accept u_params")
+    except AttributeError:
+        pass
+
+
+def test_get_unbounded_tburstpop_params_fails_when_passing_u_params():
+    try:
+        tbp.get_unbounded_tburstpop_params(tbp.DEFAULT_TBURSTPOP_U_PARAMS)
+        raise NameError("get_unbounded_tburstpop_params should not accept u_params")
+    except AttributeError:
+        pass
+
+
+def test_get_tburst_params_from_tburstpop_params_fails_when_passing_u_params():
+    logsm, logssfr = 10.0, -11.0
+
+    try:
+        tbp.get_tburst_params_from_tburstpop_params(
+            tbp.DEFAULT_TBURSTPOP_U_PARAMS, logsm, logssfr
+        )
+        raise NameError(
+            "get_tburst_params_from_tburstpop_params should not accept u_params"
+        )
+    except AttributeError:
+        pass
+
+
+def test_get_tburst_params_from_tburstpop_u_params_fails_when_passing_params():
+    logsm, logssfr = 10.0, -11.0
+
+    try:
+        tbp.get_tburst_params_from_tburstpop_u_params(
+            tbp.DEFAULT_TBURSTPOP_PARAMS, logsm, logssfr
+        )
+        raise NameError(
+            "get_tburst_params_from_tburstpop_u_params should not accept params"
+        )
+    except AttributeError:
+        pass
+
+
+def test_get_tburst_u_params_from_tburstpop_params_fails_when_passing_u_params():
+    logsm, logssfr = 10.0, -11.0
+
+    try:
+        tbp.get_tburst_u_params_from_tburstpop_params(
+            tbp.DEFAULT_TBURSTPOP_U_PARAMS, logsm, logssfr
+        )
+        raise NameError(
+            "get_tburst_u_params_from_tburstpop_params should not accept u_params"
+        )
+    except AttributeError:
+        pass
+
+
+def test_get_tburst_u_params_from_tburstpop_u_params_fails_when_passing_params():
+    logsm, logssfr = 10.0, -11.0
+
+    try:
+        tbp.get_tburst_u_params_from_tburstpop_u_params(
+            tbp.DEFAULT_TBURSTPOP_PARAMS,
+            logsm,
+            logssfr,
+        )
+        raise NameError(
+            "get_tburst_u_params_from_tburstpop_u_params should not accept u_params"
+        )
+    except AttributeError:
+        pass
+
+
+def test_get_tburst_u_params_from_tburstpop_params_evaluates():
+    ran_key = jran.PRNGKey(0)
+    ran_key, logsm_key, logssfr_key = jran.split(ran_key, 3)
+    logsm = jran.uniform(logsm_key, minval=0, maxval=10, shape=())
+    logssfr = jran.uniform(logssfr_key, minval=-12, maxval=-8, shape=())
+
+    n_age = 107
+    lgyr = np.linspace(5.5, 10.25, n_age)
+
+    u_lgyr_peak, u_lgyr_max = tbp.get_tburst_u_params_from_tburstpop_params(
+        tbp.DEFAULT_TBURSTPOP_PARAMS,
+        logsm,
+        logssfr,
+    )
+    assert u_lgyr_peak.shape == ()
+    assert u_lgyr_max.shape == ()
+
+    age_weights = _pureburst_age_weights_from_u_params(lgyr, u_lgyr_peak, u_lgyr_max)
+    assert age_weights.shape == (n_age,)
+    assert np.all(np.isfinite(age_weights))
+    assert np.allclose(np.sum(age_weights), 1.0, rtol=1e-4)
+    assert np.all(age_weights <= 1)
+    assert np.all(age_weights >= 0)
+
+
+def test_get_tburst_params_from_tburstpop_params_evaluates():
+    ran_key = jran.PRNGKey(0)
+    ran_key, logsm_key, logssfr_key = jran.split(ran_key, 3)
+    logsm = jran.uniform(logsm_key, minval=0, maxval=10, shape=())
+    logssfr = jran.uniform(logssfr_key, minval=-12, maxval=-8, shape=())
+
+    n_age = 107
+    lgyr = np.linspace(5.5, 10.25, n_age)
+
+    lgyr_peak, lgyr_max = tbp.get_tburst_params_from_tburstpop_params(
+        tbp.DEFAULT_TBURSTPOP_PARAMS,
+        logsm,
+        logssfr,
+    )
+    assert lgyr_peak.shape == ()
+    assert lgyr_max.shape == ()
+
+    age_weights = _pureburst_age_weights_from_params(lgyr, lgyr_peak, lgyr_max)
+    assert age_weights.shape == (n_age,)
+    assert np.all(np.isfinite(age_weights))
+    assert np.allclose(np.sum(age_weights), 1.0, rtol=1e-4)
+    assert np.all(age_weights <= 1)
+    assert np.all(age_weights >= 0)
+
+
+def test_get_tburst_params_from_tburstpop_u_params_evaluates():
+    ran_key = jran.PRNGKey(0)
+    ran_key, logsm_key, logssfr_key = jran.split(ran_key, 3)
+    logsm = jran.uniform(logsm_key, minval=0, maxval=10, shape=())
+    logssfr = jran.uniform(logssfr_key, minval=-12, maxval=-8, shape=())
+
+    n_age = 107
+    lgyr = np.linspace(5.5, 10.25, n_age)
+
+    lgyr_peak, lgyr_max = tbp.get_tburst_params_from_tburstpop_u_params(
+        tbp.DEFAULT_TBURSTPOP_U_PARAMS, logsm, logssfr
+    )
+    assert lgyr_peak.shape == ()
+    assert lgyr_max.shape == ()
+
+    age_weights = _pureburst_age_weights_from_params(lgyr, lgyr_peak, lgyr_max)
+    assert age_weights.shape == (n_age,)
+    assert np.all(np.isfinite(age_weights))
+    assert np.allclose(np.sum(age_weights), 1.0, rtol=1e-4)
+    assert np.all(age_weights <= 1)
+    assert np.all(age_weights >= 0)
+
+
+def test_get_tburst_params_from_tburstpop_u_params_are_consistent():
+    ran_key = jran.PRNGKey(0)
+    ran_key, logsm_key, logssfr_key = jran.split(ran_key, 3)
+    logsm = jran.uniform(logsm_key, minval=0, maxval=10, shape=())
+    logssfr = jran.uniform(logssfr_key, minval=-12, maxval=-8, shape=())
+
+    n_age = 107
+    lgyr = np.linspace(5.5, 10.25, n_age)
+
+    lgyr_peak1, lgyr_max1 = tbp.get_tburst_params_from_tburstpop_u_params(
+        tbp.DEFAULT_TBURSTPOP_U_PARAMS, logsm, logssfr
+    )
+    age_weights1 = _pureburst_age_weights_from_params(lgyr, lgyr_peak1, lgyr_max1)
+
+    lgyr_peak2, lgyr_max2 = tbp.get_tburst_params_from_tburstpop_params(
+        tbp.DEFAULT_TBURSTPOP_PARAMS, logsm, logssfr
+    )
+    age_weights2 = _pureburst_age_weights_from_params(lgyr, lgyr_peak2, lgyr_max2)
+
+    assert np.allclose(lgyr_peak1, lgyr_peak2)
+    assert np.allclose(lgyr_max1, lgyr_max2)
+    assert np.allclose(age_weights1, age_weights2, rtol=TOL)
+
+
+def test_get_tburst_u_params_from_tburstpop_u_params_are_consistent():
+    ran_key = jran.PRNGKey(0)
+    ran_key, logsm_key, logssfr_key = jran.split(ran_key, 3)
+    logsm = jran.uniform(logsm_key, minval=0, maxval=10, shape=())
+    logssfr = jran.uniform(logssfr_key, minval=-12, maxval=-8, shape=())
+
+    n_age = 107
+    lgyr = np.linspace(5.5, 10.25, n_age)
+
+    u_lgyr_peak1, u_lgyr_max1 = tbp.get_tburst_u_params_from_tburstpop_u_params(
+        tbp.DEFAULT_TBURSTPOP_U_PARAMS, logsm, logssfr
+    )
+    age_weights1 = _pureburst_age_weights_from_u_params(lgyr, u_lgyr_peak1, u_lgyr_max1)
+
+    u_lgyr_peak2, u_lgyr_max2 = tbp.get_tburst_u_params_from_tburstpop_params(
+        tbp.DEFAULT_TBURSTPOP_PARAMS, logsm, logssfr
+    )
+    age_weights2 = _pureburst_age_weights_from_u_params(lgyr, u_lgyr_peak2, u_lgyr_max2)
+
+    assert np.allclose(u_lgyr_peak1, u_lgyr_peak2, rtol=1e-2)
+    assert np.allclose(u_lgyr_max1, u_lgyr_max2, rtol=1e-2)
+    assert np.allclose(age_weights1, age_weights2, rtol=1e-2)
+
+
+def test_tburstpop_param_u_param_inversion():
+    assert np.allclose(
+        tbp.DEFAULT_TBURSTPOP_PARAMS,
+        tbp.get_bounded_tburstpop_params(tbp.DEFAULT_TBURSTPOP_U_PARAMS),
+        atol=0.05,
+    )
+
+    inferred_default_params = tbp.get_bounded_tburstpop_params(
+        tbp.get_unbounded_tburstpop_params(tbp.DEFAULT_TBURSTPOP_PARAMS)
+    )
+    assert np.allclose(tbp.DEFAULT_TBURSTPOP_PARAMS, inferred_default_params, rtol=TOL)
+
+    n_gals = 10_000
+    ran_key = jran.PRNGKey(0)
+    ran_key, logsm_key, logssfr_key = jran.split(ran_key, 3)
+    logsm = jran.uniform(logsm_key, minval=0, maxval=10, shape=(n_gals,))
+    logssfr = jran.uniform(logssfr_key, minval=-12, maxval=-8, shape=(n_gals,))
+
+    u_lgyr_peak, u_lgyr_max = tbp.get_tburst_u_params_from_tburstpop_params(
+        tbp.DEFAULT_TBURSTPOP_PARAMS, logsm, logssfr
+    )
+    assert u_lgyr_peak.shape == (n_gals,)
+    assert u_lgyr_max.shape == (n_gals,)
+    assert np.all(np.isfinite(u_lgyr_peak))
+    assert np.all(np.isfinite(u_lgyr_max))
+
+    u_lgyr_peak_u, u_lgyr_max_u = tbp.get_tburst_u_params_from_tburstpop_u_params(
+        tbp.DEFAULT_TBURSTPOP_U_PARAMS, logsm, logssfr
+    )
+    assert u_lgyr_peak_u.shape == (n_gals,)
+    assert u_lgyr_max_u.shape == (n_gals,)
+    assert np.all(np.isfinite(u_lgyr_peak_u))
+    assert np.all(np.isfinite(u_lgyr_max_u))
+
+    assert np.allclose(u_lgyr_max, u_lgyr_max_u, atol=0.05)
+    assert np.allclose(u_lgyr_peak, u_lgyr_peak_u, atol=0.05)
