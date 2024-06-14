5d1177e2b627d14865b0402f11f161e14fcf278a
Andrew Hearin <ahearin@anl.gov>
2024-06-14 07:36:41 -0500

    lgav --> suav

--- diffsky/dustpop/avpop_mono.py
+++ diffsky/dustpop/avpop_mono.py
@@ -20,38 +20,38 @@ LGSM_X0 = 10.0
 LGSSFR_X0 = -10.5
 
 DEFAULT_AVPOP_PDICT = OrderedDict(
-    lgav_logsm_x0=10.0,
-    lgav_logssfr_x0=-10.25,
-    lgav_logsm_ylo_q_z_ylo=-0.25,
-    lgav_logsm_ylo_ms_z_ylo=-0.25,
-    lgav_logsm_ylo_q_z_yhi=-0.25,
-    lgav_logsm_ylo_ms_z_yhi=-0.25,
-    lgav_logsm_yhi_q_z_ylo=-0.25,
-    lgav_logsm_yhi_ms_z_ylo=-0.25,
-    lgav_logsm_yhi_q_z_yhi=-0.25,
-    lgav_logsm_yhi_ms_z_yhi=-0.25,
-    lgav_z_x0=1.0,
+    suav_logsm_x0=10.0,
+    suav_logssfr_x0=-10.25,
+    suav_logsm_ylo_q_z_ylo=-0.25,
+    suav_logsm_ylo_ms_z_ylo=-0.25,
+    suav_logsm_ylo_q_z_yhi=-0.25,
+    suav_logsm_ylo_ms_z_yhi=-0.25,
+    suav_logsm_yhi_q_z_ylo=-0.25,
+    suav_logsm_yhi_ms_z_ylo=-0.25,
+    suav_logsm_yhi_q_z_yhi=-0.25,
+    suav_logsm_yhi_ms_z_yhi=-0.25,
+    suav_z_x0=1.0,
     delta_u_av=0.2,
 )
 
-LGSM_X0_BOUNDS = (8.0, 12.0)
-LGSSFR_X0_BOUNDS = (-13.0, -7.0)
-LGAV_BOUNDS = (-4.0, 1.5)
+LGSM_X0_BOUNDS = (9.0, 11.0)
+LGSSFR_X0_BOUNDS = (-12.0, -8.0)
+SUAV_BOUNDS = (-3.0, 3.0)
 REDSHIFT_BOUNDS = (0.0, 5.0)
 DELTA_U_AV_BOUNDS = (0.0, 1.0)
 
 AVPOP_PBOUNDS_PDICT = OrderedDict(
-    lgav_logsm_x0=LGSM_X0_BOUNDS,
-    lgav_logssfr_x0=LGSSFR_X0_BOUNDS,
-    lgav_logsm_ylo_q_z_ylo=LGAV_BOUNDS,
-    lgav_logsm_ylo_ms_z_ylo=LGAV_BOUNDS,
-    lgav_logsm_ylo_q_z_yhi=LGAV_BOUNDS,
-    lgav_logsm_ylo_ms_z_yhi=LGAV_BOUNDS,
-    lgav_logsm_yhi_q_z_ylo=LGAV_BOUNDS,
-    lgav_logsm_yhi_ms_z_ylo=LGAV_BOUNDS,
-    lgav_logsm_yhi_q_z_yhi=LGAV_BOUNDS,
-    lgav_logsm_yhi_ms_z_yhi=LGAV_BOUNDS,
-    lgav_z_x0=REDSHIFT_BOUNDS,
+    suav_logsm_x0=LGSM_X0_BOUNDS,
+    suav_logssfr_x0=LGSSFR_X0_BOUNDS,
+    suav_logsm_ylo_q_z_ylo=SUAV_BOUNDS,
+    suav_logsm_ylo_ms_z_ylo=SUAV_BOUNDS,
+    suav_logsm_ylo_q_z_yhi=SUAV_BOUNDS,
+    suav_logsm_ylo_ms_z_yhi=SUAV_BOUNDS,
+    suav_logsm_yhi_q_z_ylo=SUAV_BOUNDS,
+    suav_logsm_yhi_ms_z_ylo=SUAV_BOUNDS,
+    suav_logsm_yhi_q_z_yhi=SUAV_BOUNDS,
+    suav_logsm_yhi_ms_z_yhi=SUAV_BOUNDS,
+    suav_z_x0=REDSHIFT_BOUNDS,
     delta_u_av=DELTA_U_AV_BOUNDS,
 )
 
@@ -68,54 +68,54 @@ AVPOP_PBOUNDS = AvPopParams(**AVPOP_PBOUNDS_PDICT)
 
 @jjit
 def get_av_from_avpop_params_scalar(avpop_params, logsm, logssfr, redshift, lg_age_gyr):
-    lgav_logssfr_q_z_ylo = _sigmoid(
+    suav_logssfr_q_z_ylo = _sigmoid(
         logsm,
-        avpop_params.lgav_logsm_x0,
+        avpop_params.suav_logsm_x0,
         LGSM_K,
-        avpop_params.lgav_logsm_ylo_q_z_ylo,
-        avpop_params.lgav_logsm_yhi_q_z_ylo,
+        avpop_params.suav_logsm_ylo_q_z_ylo,
+        avpop_params.suav_logsm_yhi_q_z_ylo,
     )
-    lgav_logssfr_ms_z_ylo = _sigmoid(
+    suav_logssfr_ms_z_ylo = _sigmoid(
         logsm,
-        avpop_params.lgav_logsm_x0,
+        avpop_params.suav_logsm_x0,
         LGSSFR_K,
-        avpop_params.lgav_logsm_ylo_ms_z_ylo,
-        avpop_params.lgav_logsm_yhi_ms_z_ylo,
+        avpop_params.suav_logsm_ylo_ms_z_ylo,
+        avpop_params.suav_logsm_yhi_ms_z_ylo,
     )
 
-    lgav_z_ylo = _sigmoid(
+    suav_z_ylo = _sigmoid(
         logssfr,
-        avpop_params.lgav_logssfr_x0,
+        avpop_params.suav_logssfr_x0,
         LGSSFR_K,
-        lgav_logssfr_q_z_ylo,
-        lgav_logssfr_ms_z_ylo,
+        suav_logssfr_q_z_ylo,
+        suav_logssfr_ms_z_ylo,
     )
 
-    lgav_logssfr_q_z_yhi = _sigmoid(
+    suav_logssfr_q_z_yhi = _sigmoid(
         logsm,
-        avpop_params.lgav_logsm_x0,
+        avpop_params.suav_logsm_x0,
         LGSM_K,
-        avpop_params.lgav_logsm_ylo_q_z_yhi,
-        avpop_params.lgav_logsm_yhi_q_z_yhi,
+        avpop_params.suav_logsm_ylo_q_z_yhi,
+        avpop_params.suav_logsm_yhi_q_z_yhi,
     )
-    lgav_logssfr_ms_z_yhi = _sigmoid(
+    suav_logssfr_ms_z_yhi = _sigmoid(
         logsm,
-        avpop_params.lgav_logsm_x0,
+        avpop_params.suav_logsm_x0,
         LGSSFR_K,
-        avpop_params.lgav_logsm_ylo_ms_z_yhi,
-        avpop_params.lgav_logsm_yhi_ms_z_yhi,
+        avpop_params.suav_logsm_ylo_ms_z_yhi,
+        avpop_params.suav_logsm_yhi_ms_z_yhi,
     )
 
-    lgav_z_yhi = _sigmoid(
+    suav_z_yhi = _sigmoid(
         logssfr,
-        avpop_params.lgav_logssfr_x0,
+        avpop_params.suav_logssfr_x0,
         LGSSFR_K,
-        lgav_logssfr_q_z_yhi,
-        lgav_logssfr_ms_z_yhi,
+        suav_logssfr_q_z_yhi,
+        suav_logssfr_ms_z_yhi,
     )
 
     u_av = _sigmoid(
-        redshift, avpop_params.lgav_z_x0, REDSHIFT_K, lgav_z_ylo, lgav_z_yhi
+        redshift, avpop_params.suav_z_x0, REDSHIFT_K, suav_z_ylo, suav_z_yhi
     )
 
     delta_u_av = _young_star_av_boost_kern(lg_age_gyr, avpop_params.u_av_boost_ostars)
@@ -132,22 +132,22 @@ def _young_star_av_boost_kern(lg_age_gyr, u_av_boost_ostars):
 
 
 @jjit
-def _get_bounded_lgav_param(u_param, bound):
+def _get_bounded_suav_param(u_param, bound):
     lo, hi = bound
     mid = 0.5 * (lo + hi)
     return _sigmoid(u_param, mid, 0.1, lo, hi)
 
 
 @jjit
-def _get_unbounded_lgav_param(param, bound):
+def _get_unbounded_suav_param(param, bound):
     lo, hi = bound
     mid = 0.5 * (lo + hi)
     return _inverse_sigmoid(param, mid, 0.1, lo, hi)
 
 
 _C = (0, 0)
-_get_avpop_params_kern = jjit(vmap(_get_bounded_lgav_param, in_axes=_C))
-_get_lgav_u_params_kern = jjit(vmap(_get_unbounded_lgav_param, in_axes=_C))
+_get_avpop_params_kern = jjit(vmap(_get_bounded_suav_param, in_axes=_C))
+_get_suav_u_params_kern = jjit(vmap(_get_unbounded_suav_param, in_axes=_C))
 
 
 @jjit
@@ -162,7 +162,7 @@ def get_unbounded_avpop_params(params):
     params = jnp.array(
         [getattr(params, pname) for pname in DEFAULT_AVPOP_PARAMS._fields]
     )
-    u_params = _get_lgav_u_params_kern(jnp.array(params), jnp.array(AVPOP_PBOUNDS))
+    u_params = _get_suav_u_params_kern(jnp.array(params), jnp.array(AVPOP_PBOUNDS))
     return AvPopUParams(*u_params)
 
 
