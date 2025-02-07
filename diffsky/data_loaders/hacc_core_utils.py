6b26008abe8a103fa30e1b8c243cf763a7e520ca
Andrew Hearin <ahearin@anl.gov>
2025-02-07 14:57:56 -0600

    Add test of get_timestep_range_from_z_range function

--- diffsky/data_loaders/hacc_core_utils.py
+++ diffsky/data_loaders/hacc_core_utils.py
@@ -88,7 +88,23 @@ def get_diffstar_cosmo_quantities(sim_name):
 
 
 def get_timestep_range_from_z_range(sim_name, z_min, z_max):
-    """Find the HACC timesteps that contains the redshift range [z_min, z_max]"""
+    """Find the HACC timesteps that contains the redshift range [z_min, z_max]
+
+    Parameters
+    ----------
+    sim_name : HACCSim name
+
+    z_min, z_max : floats
+
+    Returns
+    -------
+    idx_step_min, idx_step_max : ints
+        Indices of the timestep array that span the input z-range
+
+    timestep_min, timestep_max : ints
+        Timesteps that span the input z-range
+
+    """
     sim = HACCSim.simulations[sim_name]
     timesteps = np.array(sim.cosmotools_steps)
 
@@ -96,8 +112,8 @@ def get_timestep_range_from_z_range(sim_name, z_min, z_max):
     a_min = 1 / (1 + z_max)
     a_arr = sim.step2a(timesteps)
 
-    idx_a_min = np.searchsorted(a_arr, a_min) - 1
-    idx_a_max = np.searchsorted(a_arr, a_max) - 1
-    timestep_min = timesteps[idx_a_min]
-    timestep_max = timesteps[idx_a_max]
-    return timestep_min, timestep_max
+    idx_step_min = np.searchsorted(a_arr, a_min) - 1
+    idx_step_max = np.searchsorted(a_arr, a_max)
+    timestep_min = timesteps[idx_step_min]
+    timestep_max = timesteps[idx_step_max]
+    return idx_step_min, idx_step_max, timestep_min, timestep_max
--- diffsky/data_loaders/tests/test_hacc_core_utils.py
+++ diffsky/data_loaders/tests/test_hacc_core_utils.py
@@ -1,16 +1,60 @@
 """
 """
 
+import numpy as np
 import pytest
+from jax import random as jran
 
 from .. import hacc_core_utils as hcu
 
+try:
+    from haccytrees import Simulation as HACCSim
+
+    HAS_HACCYTREES = True
+except ImportError:
+    HAS_HACCYTREES = False
 NO_HACC_MSG = "Must have haccytrees installed to run this test"
 
 
-@pytest.mark.skipif(not hcu.HAS_HACCYTREES, reason=NO_HACC_MSG)
+@pytest.mark.skipif(not HAS_HACCYTREES, reason=NO_HACC_MSG)
 def test_get_diffstar_cosmo_quantities():
     sim_name = "LastJourney"
     fb, lgt0 = hcu.get_diffstar_cosmo_quantities(sim_name)
     assert 0.1 < fb < 0.2
     assert 1.1 < lgt0 < 1.2
+
+
+@pytest.mark.skipif(not HAS_HACCYTREES, reason=NO_HACC_MSG)
+def test_get_timestep_range_from_z_range():
+    sim_name = "LastJourney"
+    sim = HACCSim.simulations[sim_name]
+    timesteps = np.array(sim.cosmotools_steps)
+    z_arr = sim.step2z(timesteps)
+
+    Z_MIN, Z_MAX = z_arr.min() + 0.02, z_arr.max() - 0.02
+
+    ran_key = jran.key(0)
+    n_tests = 1000
+    for __ in range(n_tests):
+        ran_key, test_key = jran.split(ran_key, 2)
+        z_min, z_max = jran.uniform(test_key, minval=Z_MIN, maxval=Z_MAX, shape=(2,))
+        z_min, z_max = np.sort((z_min, z_max))
+
+        _res = hcu.get_timestep_range_from_z_range(sim_name, z_min, z_max)
+        idx_step_min, idx_step_max, timestep_min, timestep_max = _res
+
+        # Enforce z_min is contained in the step range
+        snap_z_lo = z_arr[idx_step_max]
+        assert z_min >= snap_z_lo
+
+        # Enforce z_max is contained in the step range
+        snap_z_hi = z_arr[idx_step_min]
+        assert z_max <= snap_z_hi
+
+        # Enforce the next timestep would have overshot z_min
+        if idx_step_max < z_arr.size - 1:
+            assert z_min < z_arr[idx_step_max - 1]
+
+        # Enforce the next timestep would have undershot z_max
+        if idx_step_max > 0:
+            assert z_max > z_arr[idx_step_min + 1]
