712762b48b45986b4626ce4b7cb1fec7939baf26
Andrew Hearin <ahearin@anl.gov>
2025-02-25 13:19:04 -0600

    Add new module to define stellar mass for cosmos

---
+++ diffsky/cosmos_utils/cosmos_mstar_model.py
@@ -0,0 +1,134 @@
+"""
+"""
+
+from collections import OrderedDict, namedtuple
+
+import numpy as np
+from cosmos20_colors import load_cosmos20
+from jax import jit as jjit
+from jax import numpy as jnp
+from jax import value_and_grad
+from jax.example_libraries import optimizers as jax_opt
+
+DEFAULT_PDICT = OrderedDict(
+    b0=18.01,
+    i=-0.45,
+    gr=0.37,
+    ri=0.59,
+    iz=1.00,
+    redshift=1.25,
+)
+ModelParams = namedtuple("ModelParams", DEFAULT_PDICT.keys())
+DEFAULT_PARAMS = ModelParams(*list(DEFAULT_PDICT.values()))
+
+data_keys = list(DEFAULT_PDICT.keys())[1:]
+PhotData = namedtuple("PhotData", data_keys + ["logsm"])
+
+
+COSMOS_PHOT_KEYS = (
+    "HSC_g_MAG",
+    "HSC_r_MAG",
+    "HSC_i_MAG",
+    "HSC_z_MAG",
+    "HSC_y_MAG",
+)
+
+
+def load_cosmos20_tdata(
+    phot_keys=COSMOS_PHOT_KEYS, zlo=0.5, zhi=2.5, sm_key="lp_mass_best", **kwargs
+):
+    cosmos = load_cosmos20(**kwargs)
+    msk_goodphot = np.ones(len(cosmos)).astype(bool)
+    for key in phot_keys:
+        msk_goodphot = msk_goodphot & np.isfinite(cosmos[key])
+    msk_z = (cosmos["photoz"] >= zlo) & (cosmos["photoz"] <= zhi)
+    msk_sm = np.isfinite(cosmos[sm_key])
+
+    msk = msk_goodphot & msk_z & msk_sm
+    cat = dict()
+    cat["photoz"] = np.copy(cosmos[msk]["photoz"])
+    cat["logsm"] = np.copy(cosmos[msk][sm_key])
+
+    for key in phot_keys:
+        newkey = key.split("_")[1]
+        cat[newkey] = np.copy(cosmos[msk][key])
+
+    gr = cat["g"] - cat["r"]
+    ri = cat["r"] - cat["i"]
+    iz = cat["i"] - cat["z"]
+
+    photdata = PhotData(cat["i"], gr, ri, iz, cat["photoz"], cat["logsm"])
+    return photdata
+
+
+@jjit
+def predict_logsm(params, photdata):
+    logsm = (
+        params.b0
+        + params.i * photdata.i
+        + params.gr * photdata.gr
+        + params.ri * photdata.ri
+        + params.iz * photdata.iz
+        + params.redshift * photdata.redshift
+    )
+    return logsm
+
+
+@jjit
+def _mse(x, y):
+    diff = y - x
+    return jnp.mean(diff * diff)
+
+
+@jjit
+def _mae(x, y):
+    diff = y - x
+    return jnp.mean(jnp.abs(diff))
+
+
+@jjit
+def loss_kern(params, photdata):
+    pred = predict_logsm(params, photdata)
+    target = photdata.logsm
+    return _mae(pred, target)
+
+
+loss_and_grad_func = jjit(value_and_grad(loss_kern))
+
+
+def fit_model(n_steps, photdata, params_init=DEFAULT_PARAMS, step_size=0.001):
+    loss_collector = []
+    opt_init, opt_update, get_params = jax_opt.adam(step_size)
+    opt_state = opt_init(params_init)
+    params = get_params(opt_state)
+
+    loss_init, grads = loss_and_grad_func(params_init, photdata)
+
+    for istep in range(n_steps):
+        params = get_params(opt_state)
+        loss, grads = loss_and_grad_func(params, photdata)
+        opt_state = opt_update(istep, grads, opt_state)
+        loss_collector.append(loss)
+
+    loss_arr = np.array(loss_collector)
+    best_fit_params = get_params(opt_state)
+
+    return best_fit_params, loss_arr
+
+
+if __name__ == "__main__":
+    photdata = load_cosmos20_tdata()
+
+    p_best, loss_arr_init = fit_model(100, photdata, step_size=0.01)
+    p_best, loss_arr_init2 = fit_model(
+        100, photdata, params_init=p_best, step_size=0.01
+    )
+    p_best, loss_arr0 = fit_model(1_000, photdata, params_init=p_best, step_size=0.1)
+    p_best, loss_arr1 = fit_model(1_000, photdata, params_init=p_best, step_size=0.01)
+    loss_arr = np.concatenate((loss_arr_init, loss_arr_init2, loss_arr0, loss_arr1))
+
+    print("Best fit model:\n")
+    for p, key in zip(p_best, p_best._fields):
+        print(f"{key}={p:.2f},")
+
+    np.save("loss_history", loss_arr)
---
+++ diffsky/cosmos_utils/tests/test_cosmos_mstar_model.py
@@ -0,0 +1,36 @@
+"""
+"""
+
+import numpy as np
+from jax import random as jran
+
+from .. import cosmos_mstar_model as cmm
+
+
+def get_fake_cosmos_data(ran_key, n=2_000):
+    collector = []
+    for field in cmm.PhotData._fields:
+        ran_key, field_key = jran.split(ran_key, 2)
+        arr = jran.uniform(field_key, minval=0, maxval=2, shape=(n,))
+        collector.append(arr)
+    photdata = cmm.PhotData(*collector)
+    return photdata
+
+
+def test_fit_model():
+    ran_key = jran.key(0)
+
+    try:
+        photdata = cmm.load_cosmos20_tdata()
+    except OSError:
+        photdata = get_fake_cosmos_data(ran_key)
+
+    logsm = cmm.predict_logsm(cmm.DEFAULT_PARAMS, photdata)
+    loss_init = cmm.loss_kern(cmm.DEFAULT_PARAMS, photdata)
+    assert np.all(np.isfinite(logsm))
+    assert np.all(np.isfinite(loss_init))
+
+    p_best, loss_arr = cmm.fit_model(100, photdata, step_size=0.01)
+
+    loss_best = cmm.loss_kern(p_best, photdata)
+    assert loss_best < loss_init
