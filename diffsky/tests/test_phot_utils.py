775f28ed9ff8803f296ea478604940ed356c80cd
Andrew Hearin <ahearin@anl.gov>
2025-03-05 14:27:02 -0600

    Working refactoring of phot_utils and call to photometry kernels

--- diffsky/tests/test_phot_utils.py
+++ diffsky/tests/test_phot_utils.py
@@ -4,20 +4,37 @@
 import os
 
 import numpy as np
+import pytest
 from dsps.data_loaders import retrieve_fake_fsps_data as rffd
+from dsps.data_loaders.load_ssp_data import load_ssp_templates
 
 from .. import phot_utils as pu
 
 try:
-    DEFAULT_DSPS_DRN = os.environ["DSPS_DRN"]
+    DSPS_DATA_DRN = os.environ["DSPS_DRN"]
+    HAS_REAL_DSPS_DATA = True
 except KeyError:
-    DEFAULT_DSPS_DRN = ""
+    DSPS_DATA_DRN = ""
+    HAS_REAL_DSPS_DATA = False
+HAS_DATA_MSG = "Must have DSPS_DRN in os.environ to run this test"
 
 
-def test_get_interpolated_lsst_tcurves():
+def test_load_fake_interpolated_lsst_tcurves():
     ssp_data = rffd.load_fake_ssp_data()
-    tcurves = pu.get_interpolated_lsst_tcurves(
-        ssp_data.ssp_wave, drn_ssp_data=DEFAULT_DSPS_DRN
+    tcurves = pu.load_interpolated_lsst_curves(ssp_data.ssp_wave, drn_ssp_data=None)
+    assert len(tcurves) == 6
+    for tcurve in tcurves:
+        assert np.all(np.isfinite(tcurve.wave))
+        assert np.all(np.isfinite(tcurve.transmission))
+        assert np.all(tcurve.transmission >= 0)
+        assert np.all(tcurve.transmission <= 1)
+
+
+@pytest.mark.skipif(not HAS_REAL_DSPS_DATA, reason=HAS_DATA_MSG)
+def test_load_real_interpolated_lsst_tcurves():
+    ssp_data = load_ssp_templates(drn=DSPS_DATA_DRN)
+    tcurves = pu.load_interpolated_lsst_curves(
+        ssp_data.ssp_wave, drn_ssp_data=DSPS_DATA_DRN
     )
     assert len(tcurves) == 6
     for tcurve in tcurves:
