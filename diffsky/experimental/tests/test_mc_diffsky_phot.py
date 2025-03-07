474acb745e3793a3a6ffa4e3d511723f0f35489f
Andrew Hearin <ahearin@anl.gov>
2025-03-07 11:05:04 -0600

    Use refactored code to write mc_diffsky_cenpop_lsst_phot

--- diffsky/experimental/tests/test_mc_diffsky_phot.py
+++ diffsky/experimental/tests/test_mc_diffsky_phot.py
@@ -13,11 +13,12 @@ def test_mc_diffsky_galpop_lsst_phot():
     lgmp_min = 12.0
     z_obs = 0.2
     Lbox = 20.0
+    volume_com = Lbox**3
     ssp_data = rffd.load_fake_ssp_data()
 
-    args = (ran_key, z_obs, lgmp_min, ssp_data)
+    args = (ran_key, z_obs, lgmp_min, volume_com, ssp_data)
     diffsky_data = mcdp.mc_diffsky_galpop_lsst_phot(
-        *args, volume_com=Lbox**3, drn_ssp_data=None, return_internal_quantities=True
+        *args, drn_ssp_data=None, return_internal_quantities=True
     )
     for key in diffsky_data.keys():
         if "rest_ugrizy" in key:
@@ -29,3 +30,28 @@ def test_mc_diffsky_galpop_lsst_phot():
     assert np.all(np.isfinite(diffsky_data["frac_trans_noisy"]))
     assert np.all(diffsky_data["frac_trans_noisy"] >= 0)
     assert np.all(diffsky_data["frac_trans_noisy"] <= 1)
+
+
+def test_mc_diffsky_cenpop_lsst_phot():
+    ran_key = jran.key(0)
+    z_obs = 0.2
+    hosts_logmh_at_z = np.linspace(10, 15, 300)
+    ssp_data = rffd.load_fake_ssp_data()
+
+    args = (ran_key, z_obs, hosts_logmh_at_z, ssp_data)
+    diffsky_data = mcdp.mc_diffsky_cenpop_lsst_phot(
+        *args, drn_ssp_data=None, return_internal_quantities=True
+    )
+    for key in diffsky_data.keys():
+        if "rest_ugrizy" in key:
+            assert np.all(np.isfinite(diffsky_data[key]))
+
+    assert np.all(np.isfinite(diffsky_data["frac_trans_nonoise"]))
+    assert np.all(diffsky_data["frac_trans_nonoise"] >= 0)
+    assert np.all(diffsky_data["frac_trans_nonoise"] <= 1)
+    assert np.all(np.isfinite(diffsky_data["frac_trans_noisy"]))
+    assert np.all(diffsky_data["frac_trans_noisy"] >= 0)
+    assert np.all(diffsky_data["frac_trans_noisy"] <= 1)
+
+    n_gals = diffsky_data['sfh'].shape[0]
+    assert n_gals == hosts_logmh_at_z.size
