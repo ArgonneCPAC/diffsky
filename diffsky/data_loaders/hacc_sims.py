523c8ffceea4e39368040b2280c231c39c476ec5
Andrew Hearin <ahearin@anl.gov>
2025-04-08 12:53:01 -0500

    Add new hacc_sims.py module

---
+++ diffsky/data_loaders/hacc_sims.py
@@ -0,0 +1,4 @@
+"""Global variables associated with HACC sims"""
+
+# DIFFMAH_MASS_COLNAME should be consistent with the column used in the diffmah fits
+DIFFMAH_MASS_COLNAME = "infall_tree_node_mass"
