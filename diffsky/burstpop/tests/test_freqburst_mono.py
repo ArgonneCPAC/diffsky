77c0f1bde10cf5efdeb9eb69e1b1c0832545a65c
Andrew Hearin <ahearin@anl.gov>
2025-04-29 12:17:39 -0500

    Update burst frequency model to be even less conservative

--- diffsky/burstpop/tests/test_freqburst_mono.py
+++ diffsky/burstpop/tests/test_freqburst_mono.py
@@ -200,7 +200,7 @@ def test_zeroburst_params_produce_zero_burstiness():
     assert fqb.shape == (n_gals,)
     assert np.all(np.isfinite(fqb))
     assert np.all(fqb >= 0.0)
-    assert np.all(fqb < 0.01), fqb.max()
+    assert np.all(fqb < 0.1), fqb.max()
 
     sufq_max = SUFQB_BOUNDS[1]
     fqb_max = nn.softplus(sufq_max)
