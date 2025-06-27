016d8addd231f61cbd28a8146937276cfcb0e072
Andrew Hearin <ahearin@anl.gov>
2025-06-27 11:42:36 -0500

    Refactor validation test to include new get_a_range_of_lc_cores_file function

--- diffsky/data_loaders/hacc_utils/data_validation/validate_lc_cores.py
+++ diffsky/data_loaders/hacc_utils/data_validation/validate_lc_cores.py
@@ -4,6 +4,7 @@ import os
 
 import numpy as np
 
+from .. import lightcone_utils as lcu
 from .. import load_flat_hdf5
 
 try:
@@ -18,20 +19,15 @@ BNPAT_LC_CORES = "lc_cores-{0}.{1}.hdf5"
 
 def check_zrange(fn_lc_cores, sim_name, tol=0.0002, lc_cores=None):
     """Redshift range of the data should be bounded by the expected range"""
-    sim = HACCSim.simulations[sim_name]
-    steps = np.array(sim.cosmotools_steps)
-    aarr = sim.step2a(steps)
+
+    bn_lc_cores = os.path.basename(fn_lc_cores)
+    a_min_expected, a_max_expected = lcu.get_a_range_of_lc_cores_file(
+        bn_lc_cores, sim_name
+    )
 
     if lc_cores is None:
         lc_cores = load_flat_hdf5(fn_lc_cores)
 
-    bn_lc_cores = os.path.basename(fn_lc_cores)
-    stepnum, lc_patch = [int(s) for s in bn_lc_cores.split("-")[1].split(".")[:-1]]
-    indx_step = np.searchsorted(steps, stepnum)
-
-    a_min_expected, a_max_expected = aarr[indx_step], aarr[indx_step + 1]
-
-    indx_step = np.searchsorted(steps, stepnum)
     a_min_data = lc_cores["scale_factor"].min()
     a_max_data = lc_cores["scale_factor"].max()
 
