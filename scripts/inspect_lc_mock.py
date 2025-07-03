2d4797f2a7aeb241a5c820453df4e77222142e60
Andrew Hearin <ahearin@anl.gov>
2025-07-03 08:35:55 -0500

    improve control flow of report script

--- scripts/inspect_lc_mock.py
+++ scripts/inspect_lc_mock.py
@@ -25,33 +25,40 @@ if __name__ == "__main__":
 
     all_good = True
     failure_collector = []
+    no_report_collector = []
     for fn_lc_mock in fn_lc_mock_list:
         bn_lc_mock = os.path.basename(fn_lc_mock)
 
         try:
             report = vlcm.get_lc_mock_data_report(fn_lc_mock)
+            all_good = len(report) == 0
+            if not all_good:
+                vlcm.write_lc_mock_report_to_disk(report, fn_lc_mock, drn_report)
+                print(f"{bn_lc_mock} fails readiness test")
+                failure_collector.append(bn_lc_mock)
         except OSError:
             report = dict()
             report["report_does_not_exist"] = ["Unable to generate report"]
             print(f"Unable to generate report for {bn_lc_mock}")
+            no_report_collector.append(bn_lc_mock)
 
-        all_good = len(report) == 0
-        if not all_good:
-            vlcm.write_lc_mock_report_to_disk(report, fn_lc_mock, drn_report)
-            failure_collector.append(bn_lc_mock)
-
-            if "report_does_not_exist" not in report.keys():
-                print(f"{bn_lc_mock} fails readiness test")
-
-    if len(failure_collector) == 0:
+    all_pass = (len(failure_collector) == 0) & (len(no_report_collector) == 0)
+    if all_pass:
         print("Every lc_mock data file passes all tests")
-    else:
+
+    if len(failure_collector) > 0:
         print("\nSome failures in the following lightcone patches:\n")
         for failing_bn in failure_collector:
             print(f"{failing_bn}")
 
-        fn_out = os.path.join(drn_report, "report_summary.txt")
-        with open(fn_out, "w") as fout:
-            for failing_bn in failure_collector:
-                fn_lc_mock = os.path.join(drn_mock, failing_bn)
-                fout.write(fn_lc_mock + "\n")
+    fn_out = os.path.join(drn_report, "fn_list_no_report.txt")
+    with open(fn_out, "w") as fout:
+        for no_report_bn in no_report_collector:
+            fn_lc_mock = os.path.join(drn_mock, no_report_bn)
+            fout.write(fn_lc_mock + "\n")
+
+    fn_out = os.path.join(drn_report, "fn_list_fails_readiness.txt")
+    with open(fn_out, "w") as fout:
+        for failed_bn in failure_collector:
+            fn_lc_mock = os.path.join(drn_mock, failed_bn)
+            fout.write(fn_lc_mock + "\n")
