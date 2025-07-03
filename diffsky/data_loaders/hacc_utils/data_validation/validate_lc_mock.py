7b39b8c688bffd632fc5f25de18d637d0dd4d334
Andrew Hearin <ahearin@anl.gov>
2025-07-03 07:07:30 -0500

    Add new script inspect_lc_mock.pyc

--- diffsky/data_loaders/hacc_utils/data_validation/validate_lc_mock.py
+++ diffsky/data_loaders/hacc_utils/data_validation/validate_lc_mock.py
@@ -9,12 +9,35 @@ from .. import load_flat_hdf5
 BNPAT_LC_MOCK = "lc_cores-{0}.{1}.diffsky_gals.hdf5"
 
 
-def check_all_columns_are_finite(fn):
+def get_lc_mock_data_report(fn_lc_mock):
+    report = dict()
+    data = load_flat_hdf5(fn_lc_mock, dataset="data")
+
+    msg = check_all_columns_are_finite(fn_lc_mock, data=data)
+
+    if len(msg) > 0:
+        report["finite_colums"] = msg
+
+    return report
+
+
+def write_lc_mock_report_to_disk(report, fn_lc_mock, drn_report):
+    if len(report) > 0:
+        bn_report = os.path.basename(fn_lc_mock).replace(".hdf5", ".report.txt")
+        fn_report = os.path.join(drn_report, bn_report)
+        with open(fn_report, "w") as fn_out:
+            for line in report:
+                fn_out.write(line + "\n")
+
+
+def check_all_columns_are_finite(fn_lc_mock, data=None):
+    bn = os.path.basename(fn_lc_mock)
+
+    if data is None:
+        data = load_flat_hdf5(fn_lc_mock, dataset="data")
 
     msg = []
-    mock = load_flat_hdf5(fn, dataset="data")
-    bn = os.path.basename(fn)
-    for key, arr in mock.items():
+    for key, arr in data.items():
         if not np.all(np.isfinite(arr)):
             s = f"Column {key} in {bn} has either NaN or inf"
             msg.append(s)
---
+++ scripts/inspect_lc_mock.py
@@ -0,0 +1,43 @@
+"""Script to run sanity checks on HACC lightcone mocks"""
+
+import argparse
+import os
+from glob import glob
+
+from diffsky.data_loaders.hacc_utils.data_validation import validate_lc_mock as vlcm
+
+BN_GLOBPAT_LC_MOCK = "lc_cores-*.*.diffsky_gals.hdf5"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("drn_mock", help="Directory storing lightcone mock")
+    parser.add_argument("-bnpat", help="Basename pattern", default=BN_GLOBPAT_LC_MOCK)
+    parser.add_argument("-drn_report", help="Directory to write report", default="")
+
+    args = parser.parse_args()
+    drn_mock = args.drn_mock
+    bnpat = args.bnpat
+    drn_report = args.drn_report
+
+    fn_lc_mock_list = glob(os.path.join(drn_mock, bnpat))
+
+    all_good = True
+    failure_collector = []
+    for fn_lc_mock in fn_lc_mock_list:
+        report = vlcm.get_lc_mock_data_report(fn_lc_mock)
+        all_good = len(report) == 0
+        if not all_good:
+            vlcm.write_lc_mock_report_to_disk(report, fn_lc_mock, drn_report)
+
+            bn_lc_mock = os.path.basename(fn_lc_mock)
+            print(f"{bn_lc_mock} fails readiness test")
+            failure_collector.append(bn_lc_mock)
+
+    if len(failure_collector) == 0:
+        print("Every lc_mock data file passes all tests")
+    else:
+        print("Some failures in the following lightcone patches:\n")
+        for failing_bn in failure_collector:
+            print(f"{failing_bn}")
