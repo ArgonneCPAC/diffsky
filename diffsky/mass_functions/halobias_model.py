87e63281b7f6dbbee1f4f18c46f1cab6cd48ab86
Andrew Hearin <ahearin@anl.gov>
2025-08-05 07:23:57 -0500

    Implement bounding functions for halobias_model.py

--- diffsky/mass_functions/halobias_model.py
+++ diffsky/mass_functions/halobias_model.py
@@ -4,8 +4,9 @@ from collections import OrderedDict, namedtuple
 
 from jax import jit as jjit
 
+from ..utils import _inverse_sigmoid, _sigmoid
 from ..utils import tw_utils as twu
-from . import halobias_singlez_model as hbsm
+from . import halobias_singlez_model as hbszm
 
 HALOBIAS_PDICT = OrderedDict(
     hb_ytp=0.5,
@@ -33,10 +34,72 @@ HaloBiasUParams = namedtuple("HaloBiasUParams", _HALOBIAS_UPNAMES)
 HB_YTP_PBOUNDS = (-2, 2)
 HB_YTP_X0 = 0.5
 BOUNDING_K = 0.1
+K_BOUNDS = (0.2, 5.0)
 
 
-def get_bounded_halobias_params():
-    raise NotImplementedError()
+@jjit
+def get_bounded_halobias_params(u_hb_params):
+    u_hbsz_params = hbszm.HaloBiasUParams._make(
+        [getattr(u_hb_params, x) for x in hbszm.HaloBiasUParams._fields]
+    )
+    hbsz_params = hbszm.get_bounded_halobias_params(u_hbsz_params)
+
+    hb_mz_k = _get_bounded_param_kern(u_hb_params.u_hb_mz_k, *K_BOUNDS)
+    hb_ytp_z_k = _get_bounded_param_kern(u_hb_params.u_hb_ytp_z_k, *K_BOUNDS)
+
+    params = (
+        *hbsz_params,
+        u_hb_params.u_hb_mz_x0,
+        hb_mz_k,
+        u_hb_params.u_hb_mz_lo,
+        u_hb_params.u_hb_mz_hi,
+        u_hb_params.u_hb_ytp_z_x0,
+        hb_ytp_z_k,
+        u_hb_params.u_hb_ytp_z_lo,
+        u_hb_params.u_hb_ytp_z_hi,
+    )
+    params = HaloBiasParams(*params)
+
+    return params
+
+
+@jjit
+def get_unbounded_halobias_params(hb_params):
+    hbsz_params = hbszm.HaloBiasParams._make(
+        [getattr(hb_params, x) for x in hbszm.HaloBiasParams._fields]
+    )
+    u_hbsz_params = hbszm.get_unbounded_halobias_params(hbsz_params)
+
+    u_hb_mz_k = _get_unbounded_param_kern(hb_params.hb_mz_k, *K_BOUNDS)
+    u_hb_ytp_z_k = _get_unbounded_param_kern(hb_params.hb_ytp_z_k, *K_BOUNDS)
+
+    u_params = (
+        *u_hbsz_params,
+        hb_params.hb_mz_x0,
+        u_hb_mz_k,
+        hb_params.hb_mz_lo,
+        hb_params.hb_mz_hi,
+        hb_params.hb_ytp_z_x0,
+        u_hb_ytp_z_k,
+        hb_params.hb_ytp_z_lo,
+        hb_params.hb_ytp_z_hi,
+    )
+    u_params = HaloBiasUParams(*u_params)
+    return u_params
+
+
+@jjit
+def _get_bounded_param_kern(u_p, lo, hi):
+    x0 = 0.5 * (lo + hi)
+    p = _sigmoid(u_p, x0, BOUNDING_K, lo, hi)
+    return p
+
+
+@jjit
+def _get_unbounded_param_kern(p, lo, hi):
+    x0 = 0.5 * (lo + hi)
+    u_p = _inverse_sigmoid(p, x0, BOUNDING_K, lo, hi)
+    return u_p
 
 
 @jjit
@@ -101,7 +164,10 @@ def _predict_lgbias_kern(
     hb_ytp_at_z = _get_hb_ytp_at_z(
         redshift, hb_ytp, hb_ytp_z_x0, hb_ytp_z_k, hb_ytp_z_lo, hb_ytp_z_hi
     )
-    lgbias = hbsm._tw_quintuple_sigmoid_kern(
+    lgbias = hbszm._tw_quintuple_sigmoid_kern(
         lgm_at_z, hb_ytp_at_z, hb_s0, hb_s1, hb_s2, hb_s3, hb_s4, hb_s5
     )
     return lgbias
+
+
+HALOBIAS_U_PARAMS = get_unbounded_halobias_params(HALOBIAS_PARAMS)
