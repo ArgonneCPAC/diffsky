7af6f519fd437b2609d605a1d240c90c3ea8463d
Andrew Hearin <ahearin@anl.gov>
2025-08-04 19:33:40 -0500

    Implement redshift-dependent scaling of ytp

--- diffsky/mass_functions/halobias_model.py
+++ diffsky/mass_functions/halobias_model.py
@@ -15,10 +15,10 @@ HALOBIAS_PDICT = OrderedDict(
     hb_s3=0.45,
     hb_s4=0.5,
     hb_s5=0.6,
-    hb_z_x0=1.5,
-    hb_z_k=1,
-    hb_z_lo=0.1,
-    hb_z_hi=0.1,
+    hb_mz_x0=1.5,
+    hb_mz_k=1,
+    hb_mz_lo=0.1,
+    hb_mz_hi=0.1,
 )
 HaloBiasParams = namedtuple("HaloBiasParams", HALOBIAS_PDICT.keys())
 HALOBIAS_PARAMS = HaloBiasParams(**HALOBIAS_PDICT)
@@ -36,9 +36,19 @@ def get_bounded_halobias_params():
 
 
 @jjit
-def _get_lgm_at_z_kern(lgm, z, x0, k, lo, hi):
+def _get_lgm_at_z_kern(lgm, z, hb_mz_x0, hb_mz_k, hb_mz_lo, hb_mz_hi):
     xtp, ytp = 0.0, 0.0
-    return lgm - twu._tw_sig_slope(z, xtp, ytp, x0, k, lo, hi)
+    return lgm - twu._tw_sig_slope(z, xtp, ytp, hb_mz_x0, hb_mz_k, hb_mz_lo, hb_mz_hi)
+
+
+@jjit
+def _get_hb_ytp_at_z(
+    hb_ytp_at_z0, z, hb_ytp_z_x0, hb_ytp_z_k, hb_ytp_z_lo, hb_ytp_z_hi
+):
+    xtp, ytp = 0.0, 0.0
+    return hb_ytp_at_z0 + twu._tw_sig_slope(
+        z, xtp, ytp, hb_ytp_z_x0, hb_ytp_z_k, hb_ytp_z_lo, hb_ytp_z_hi
+    )
 
 
 def predict_lgbias_kern(params, lgm, z):
@@ -56,13 +66,20 @@ def _predict_lgbias_kern(
     hb_s3,
     hb_s4,
     hb_s5,
-    hb_z_x0,
-    hb_z_k,
-    hb_z_lo,
-    hb_z_hi,
+    hb_mz_x0,
+    hb_mz_k,
+    hb_mz_lo,
+    hb_mz_hi,
+    hb_ytp_z_x0,
+    hb_ytp_z_k,
+    hb_ytp_z_lo,
+    hb_ytp_z_hi,
 ):
-    lgm_at_z = _get_lgm_at_z_kern(lgm, redshift, hb_z_x0, hb_z_k, hb_z_lo, hb_z_hi)
+    lgm_at_z = _get_lgm_at_z_kern(lgm, redshift, hb_mz_x0, hb_mz_k, hb_mz_lo, hb_mz_hi)
+    hb_ytp_at_z = _get_hb_ytp_at_z(
+        hb_ytp, redshift, hb_ytp_z_x0, hb_ytp_z_k, hb_ytp_z_lo, hb_ytp_z_hi
+    )
     lgbias = hbsm._tw_quintuple_sigmoid_kern(
-        lgm_at_z, hb_ytp, hb_s0, hb_s1, hb_s2, hb_s3, hb_s4, hb_s5
+        lgm_at_z, hb_ytp_at_z, hb_s0, hb_s1, hb_s2, hb_s3, hb_s4, hb_s5
     )
     return lgbias
