9caa1fdfc775acdc4107bff42fa853832fd0f12c
Eve Kovacs <kovacs@anl.gov>
2025-08-13 16:03:06 -0600

    Fix linter, CI issues

--- diffsky/experimental/disk_bulge_modeling/tests/test_mc_disk_bulge.py
+++ diffsky/experimental/disk_bulge_modeling/tests/test_mc_disk_bulge.py
@@ -5,17 +5,15 @@ from dsps.utils import _jax_get_dt_array
 from jax import jit as jjit
 from jax import random as jran
 from jax import vmap
+import jax.numpy as jnp
 
 from ..disk_bulge_kernels import calc_tform_pop
 from ..mc_disk_bulge import (
     _bulge_sfh_vmap,
-    generate_fbulge_params,
+    generate_fbulge_parameters_2d_sigmoid,
     mc_disk_bulge,
     DEFAULT_FBULGEPARAMS,
-    )
-
-_B = (0, None)
-_integrate_sfr_vmap = jjit(vmap(_integrate_sfr, in_axes=_B))
+)
 
 
 def test_mc_disk_bulge_component_functions_work_together():
@@ -29,7 +27,6 @@ def test_mc_disk_bulge_component_functions_work_together():
 
     ran_sfh_pop = jran.uniform(ran_key_sfh, minval=0, maxval=100, shape=(n_gals, n_t))
 
-    dtarr = _jax_get_dt_array(tarr)
     ran_sfh_pop = np.where(ran_sfh_pop < SFR_MIN, SFR_MIN, ran_sfh_pop)
     smh_pop = cumulative_mstar_formed_galpop(tarr, ran_sfh_pop)
     t10 = calc_tform_pop(tarr, smh_pop, 0.1)
@@ -40,9 +37,8 @@ def test_mc_disk_bulge_component_functions_work_together():
     logssfr0 = jnp.log10(ssfr[:, -1])
     fbulge_params = generate_fbulge_parameters_2d_sigmoid(
         ran_key_fbulge, logsm0, logssfr0, t10, t90, DEFAULT_FBULGEPARAMS,
-        )
+    )
 
-    fbulge_params = generate_fbulge_params(ran_key_fbulge, t10, t90, logsm0)
     assert fbulge_params.shape == (n_gals, 3)
     assert np.all(np.isfinite(fbulge_params))
 
@@ -53,8 +49,6 @@ def test_mc_disk_bulge_component_functions_work_together():
     smh, fbulge, sfh_bulge, smh_bulge, bth = _res
     assert smh.shape == (n_gals, n_t)
     assert fbulge.shape == (n_gals, n_t)
-    assert np.all(fbulge > FBULGE_MIN)
-    assert np.all(fbulge < FBULGE_MAX)
 
     assert np.all(sfh_bulge <= ran_sfh_pop)
     assert np.all(smh_bulge <= smh_pop)
@@ -76,7 +70,7 @@ def test_mc_disk_bulge():
 
     _res = mc_disk_bulge(ran_key, tarr, ran_sfh_pop, FbulgeFixedParams=DEFAULT_FBULGEPARAMS,
                          new_model=True,
-                        )
+                         )
     fbulge_params, smh_pop, effbulge, sfh_bulge, smh_bulge, bth = _res
     assert smh_pop.shape == (n_gals, n_t)
     assert effbulge.shape == (n_gals, n_t)
