97b9708ed647f9e18f46d16a282eaf2d3a352ac0
Eve Kovacs <kovacs@anl.gov>
2025-08-13 15:44:41 -0600

    Update disk-bulge test with new model and to use cumulative_mstar_formed_galpop

--- diffsky/experimental/disk_bulge_modeling/tests/test_mc_disk_bulge.py
+++ diffsky/experimental/disk_bulge_modeling/tests/test_mc_disk_bulge.py
@@ -1,13 +1,18 @@
 import numpy as np
-from diffstar.fitting_helpers.fitting_kernels import _integrate_sfr
+from diffstar.utils import cumulative_mstar_formed_galpop
 from dsps.constants import SFR_MIN
 from dsps.utils import _jax_get_dt_array
 from jax import jit as jjit
 from jax import random as jran
 from jax import vmap
 
-from ..disk_bulge_kernels import FBULGE_MAX, FBULGE_MIN, calc_tform_pop
-from ..mc_disk_bulge import _bulge_sfh_vmap, generate_fbulge_params, mc_disk_bulge
+from ..disk_bulge_kernels import calc_tform_pop
+from ..mc_disk_bulge import (
+    _bulge_sfh_vmap,
+    generate_fbulge_params,
+    mc_disk_bulge,
+    DEFAULT_FBULGEPARAMS,
+    )
 
 _B = (0, None)
 _integrate_sfr_vmap = jjit(vmap(_integrate_sfr, in_axes=_B))
@@ -26,10 +31,16 @@ def test_mc_disk_bulge_component_functions_work_together():
 
     dtarr = _jax_get_dt_array(tarr)
     ran_sfh_pop = np.where(ran_sfh_pop < SFR_MIN, SFR_MIN, ran_sfh_pop)
-    smh_pop = _integrate_sfr_vmap(ran_sfh_pop, dtarr)
+    smh_pop = cumulative_mstar_formed_galpop(tarr, ran_sfh_pop)
     t10 = calc_tform_pop(tarr, smh_pop, 0.1)
     t90 = calc_tform_pop(tarr, smh_pop, 0.9)
-    logsm0 = smh_pop[:, -1]
+    logsm0 = jnp.log10(smh_pop[:, -1])
+
+    ssfr = jnp.divide(ran_sfh_pop, smh_pop)
+    logssfr0 = jnp.log10(ssfr[:, -1])
+    fbulge_params = generate_fbulge_parameters_2d_sigmoid(
+        ran_key_fbulge, logsm0, logssfr0, t10, t90, DEFAULT_FBULGEPARAMS,
+        )
 
     fbulge_params = generate_fbulge_params(ran_key_fbulge, t10, t90, logsm0)
     assert fbulge_params.shape == (n_gals, 3)
@@ -63,14 +74,13 @@ def test_mc_disk_bulge():
 
     ran_sfh_pop = jran.uniform(ran_key_sfh, minval=0, maxval=100, shape=(n_gals, n_t))
 
-    _res = mc_disk_bulge(ran_key, tarr, ran_sfh_pop)
-    fbulge_params, smh_pop, fbulge, sfh_bulge, smh_bulge, bth = _res
+    _res = mc_disk_bulge(ran_key, tarr, ran_sfh_pop, FbulgeFixedParams=DEFAULT_FBULGEPARAMS,
+                         new_model=True,
+                        )
+    fbulge_params, smh_pop, effbulge, sfh_bulge, smh_bulge, bth = _res
     assert smh_pop.shape == (n_gals, n_t)
-    assert fbulge.shape == (n_gals, n_t)
+    assert effbulge.shape == (n_gals, n_t)
     assert fbulge_params.shape == (n_gals, 3)
-
-    assert np.all(fbulge > FBULGE_MIN)
-    assert np.all(fbulge < FBULGE_MAX)
 
     assert np.all(sfh_bulge <= ran_sfh_pop)
     assert np.all(smh_bulge <= smh_pop)
