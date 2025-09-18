75e8dfb5a2ac75b9683e844ef0be1287afd3ed7f
Andrew Hearin <ahearin@anl.gov>
2025-09-18 16:49:20 -0500

    Fix test_imports

--- diffsky/tests/test_imports.py
+++ diffsky/tests/test_imports.py
@@ -1,3 +1,5 @@
 """ """
 
-from ..param_utils import diffsky_param_wrapper as dpw  # noqa
+
+def test_diffsky_param_wrapper_imports():
+    from ..param_utils import diffsky_param_wrapper as dpw  # noqa
