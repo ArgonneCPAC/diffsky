a9ce0af98744ce968b711c19a73298ec9297532c
Andrew Hearin <ahearin@anl.gov>
2025-10-17 11:18:15 -0500

    Update DRN_LJ_CROSSX_OUT_LCRC to cosmo_ai location on LCRC

--- scripts/LJ_LC_MOCKS/make_sed_lc_mock_lj.py
+++ scripts/LJ_LC_MOCKS/make_sed_lc_mock_lj.py
@@ -30,7 +30,7 @@ DRN_LJ_LC_LCRC = (
 )
 DRN_LJ_LC_POBOY = "/Users/aphearin/work/DATA/LastJourney/lc_cores"
 
-DRN_LJ_CROSSX_OUT_LCRC = "/lcrc/project/halotools/LastJourney/lc-cf-diffsky"
+DRN_LJ_CROSSX_OUT_LCRC = "/lcrc/project/cosmo_ai/ahearin/LastJourney/lc-cf-diffsky"
 DRN_LC_CF_XDATA_LCRC = os.path.join(DRN_LJ_CROSSX_OUT_LCRC, "LC_CF_XDATA")
 DRN_LJ_CROSSX_OUT_POBOY = "/Users/aphearin/work/DATA/LastJourney/lc-cf-diffsky"
 DRN_LC_CF_XDATA_POBOY = os.path.join(DRN_LJ_CROSSX_OUT_POBOY, "LC_CF_XDATA")
