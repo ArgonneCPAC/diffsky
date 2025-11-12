bdc7526705fe0b195085196ec65e44f5f7885df1
Andrew Hearin <ahearin@anl.gov>
2025-11-12 07:17:27 -0600

    Add SimSpecs feature to haccsims

--- diffsky/data_loaders/hacc_utils/haccsims.py
+++ diffsky/data_loaders/hacc_utils/haccsims.py
@@ -6,10 +6,11 @@ from collections import namedtuple
 import numpy as np
 
 CosmoParams = namedtuple("CosmoParams", ("Omega_m", "w0", "wa", "h", "Omega_b"))
+SimSpecs = namedtuple("SimSpecs", ("rl", "np", "particle_mass"))
 
 _THIS_DRN = os.path.dirname(os.path.abspath(__file__))
 
-SIM_NAMES = ("LastJourney",)
+SIM_INFO = dict(LastJourney=SimSpecs(3400, 10752, 2717395894.4894614))
 
 
 class HACCSim(object):
@@ -18,6 +19,10 @@ class HACCSim(object):
         self._cosmotools_steps = None
         self._cosmo = None
         self._drn_simdata = os.path.join(_THIS_DRN, "data", sim_name)
+
+        sim_info = SIM_INFO[sim_name]
+        for key, val in zip(sim_info._fields, sim_info):
+            setattr(self, key, val)
 
     @classmethod
     def cosmotools_steps(self):
@@ -41,5 +46,5 @@ class HACCSim(object):
 
 
 simulations = dict()
-for sim_name in SIM_NAMES:
+for sim_name, sim_info in SIM_INFO.items():
     simulations[sim_name] = HACCSim(sim_name)
--- diffsky/data_loaders/hacc_utils/tests/test_haccsims.py
+++ diffsky/data_loaders/hacc_utils/tests/test_haccsims.py
@@ -1,7 +1,12 @@
 """ """
 
+import numpy as np
+
 from .. import haccsims
 
 
 def test_last_journey():
     sim = haccsims.simulations["LastJourney"]
+    assert sim.rl == 3400
+    assert np.allclose(sim.particle_mass, 10**9.434, rtol=0.01)
+    assert sim.np == 10752
