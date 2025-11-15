b84c44fd86f7d669921a830ee503d20ac675199d
Andrew Hearin <ahearin@anl.gov>
2025-11-15 10:52:22 -0600

    Fix bug in parsing of basename

--- scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
+++ scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
@@ -235,102 +235,102 @@ if __name__ == "__main__":
     rank_assignments, __ = mpi_utils.distribute_files_by_size(fn_sizes, nranks)
     fn_lc_list_for_rank = [fn_lc_list[i] for i in rank_assignments[rank]]
 
-    # if rank == 0:
-    #     print(fn_lc_list_for_rank)
+    if rank == 0:
+        print(fn_lc_list_for_rank)
 
-    # start_script = time()
-    # for fn_lc_diffsky in fn_lc_list_for_rank:
-    #     bn_lc_diffsky = os.path.join(fn_lc_diffsky)
-    #     lc_patch, stepnum = [int(x) for x in bn_lc_diffsky.split("-")[1].split(".")[:2]]
+    start_script = time()
+    for fn_lc_diffsky in fn_lc_list_for_rank:
+        gc.collect()
 
-    #     gc.collect()
+        bn_lc_diffsky = os.path.basename(fn_lc_diffsky)
+        stepnum, lc_patch = [int(x) for x in bn_lc_diffsky.split("-")[1].split(".")[:2]]
 
-    #     if rank == 0:
-    #         print(f"Working on={os.path.basename(fn_lc_diffsky)}")
+        if rank == 0:
+            print(f"Working on={os.path.basename(fn_lc_diffsky)}")
 
-    #     ran_key, patch_key = jran.split(ran_key, 2)
+        ran_key, patch_key = jran.split(ran_key, 2)
 
-    #     if synthetic_cores == 0:
-    #         lc_data, diffsky_data = load_lc_cf.load_lc_diffsky_patch_data(
-    #             fn_lc_diffsky, indir_lc_data
-    #         )
-    #     else:
-    #         bn_in = os.path.basename(fn_lc_diffsky)
-    #         bn_lc = os.path.basename(bn_in).replace(".diffsky_data.hdf5", ".hdf5")
-    #         fn_lc_cores = os.path.join(indir_lc_data, bn_lc)
-    #         patch_key, synthetic_lc_key = jran.split(patch_key, 2)
-    #         lc_data, diffsky_data = llcs.load_lc_diffsky_patch_data(
-    #             fn_lc_cores, sim_name, synthetic_lc_key, lgmp_min, lgmp_max
-    #         )
+        if synthetic_cores == 0:
+            lc_data, diffsky_data = load_lc_cf.load_lc_diffsky_patch_data(
+                fn_lc_diffsky, indir_lc_data
+            )
+        else:
+            bn_in = os.path.basename(fn_lc_diffsky)
+            bn_lc = os.path.basename(bn_in).replace(".diffsky_data.hdf5", ".hdf5")
+            fn_lc_cores = os.path.join(indir_lc_data, bn_lc)
+            patch_key, synthetic_lc_key = jran.split(patch_key, 2)
+            lc_data, diffsky_data = llcs.load_lc_diffsky_patch_data(
+                fn_lc_cores, sim_name, synthetic_lc_key, lgmp_min, lgmp_max
+            )
 
-    #     n_gals = len(lc_data["core_tag"])
-    #     lc_data["stepnum"] = np.zeros(n_gals).astype(int) + stepnum
-    #     lc_data["lc_patch"] = np.zeros(n_gals).astype(int) + lc_patch
+        n_gals = len(lc_data["core_tag"])
+        lc_data["stepnum"] = np.zeros(n_gals).astype(int) + stepnum
+        lc_data["lc_patch"] = np.zeros(n_gals).astype(int) + lc_patch
 
-    #     # Define redshift table used for magnitude interpolation
-    #     _EPS = 1e-3
-    #     z_max = lc_data["redshift_true"].max() + _EPS
+        # Define redshift table used for magnitude interpolation
+        _EPS = 1e-3
+        z_max = lc_data["redshift_true"].max() + _EPS
 
-    #     z_min_shell = lc_data["redshift_true"].min()
-    #     z_min = z_min_shell - _EPS
-    #     z_min_cutoff = 1e-3
-    #     if z_min < z_min_cutoff:
-    #         z_min = z_min_shell / 2
-    #     z_phot_table = np.linspace(z_min, z_max, n_z_phot_table)
+        z_min_shell = lc_data["redshift_true"].min()
+        z_min = z_min_shell - _EPS
+        z_min_cutoff = 1e-3
+        if z_min < z_min_cutoff:
+            z_min = z_min_shell / 2
+        z_phot_table = np.linspace(z_min, z_max, n_z_phot_table)
 
-    #     # Precompute photometry at each element of the redshift table
-    #     wave_eff_table = phot_utils.get_wave_eff_table(z_phot_table, tcurves)
+        # Precompute photometry at each element of the redshift table
+        wave_eff_table = phot_utils.get_wave_eff_table(z_phot_table, tcurves)
 
-    #     precomputed_ssp_mag_table = psspp.get_precompute_ssp_mag_redshift_table(
-    #         tcurves, ssp_data, z_phot_table, sim_info.cosmo_params
-    #     )
+        precomputed_ssp_mag_table = psspp.get_precompute_ssp_mag_redshift_table(
+            tcurves, ssp_data, z_phot_table, sim_info.cosmo_params
+        )
 
-    #     patch_key, sed_key = jran.split(patch_key, 2)
-    #     args = (
-    #         sim_info,
-    #         lc_data,
-    #         diffsky_data,
-    #         ssp_data,
-    #         param_collection,
-    #         precomputed_ssp_mag_table,
-    #         z_phot_table,
-    #         wave_eff_table,
-    #         sed_key,
-    #     )
-    #     phot_info, lc_data, diffsky_data = lcmp.add_dbk_sed_quantities_to_mock(*args)
+        patch_key, sed_key = jran.split(patch_key, 2)
+        args = (
+            sim_info,
+            lc_data,
+            diffsky_data,
+            ssp_data,
+            param_collection,
+            precomputed_ssp_mag_table,
+            z_phot_table,
+            wave_eff_table,
+            sed_key,
+        )
+        phot_info, lc_data, diffsky_data = lcmp.add_dbk_sed_quantities_to_mock(*args)
 
-    #     patch_key, morph_key = jran.split(patch_key, 2)
-    #     diffsky_data = lcmp.add_morphology_quantities_to_diffsky_data(
-    #         phot_info, lc_data, diffsky_data, morph_key
-    #     )
+        patch_key, morph_key = jran.split(patch_key, 2)
+        diffsky_data = lcmp.add_morphology_quantities_to_diffsky_data(
+            phot_info, lc_data, diffsky_data, morph_key
+        )
 
-    #     diffsky_data = lcmp.add_black_hole_quantities_to_diffsky_data(
-    #         lc_data, diffsky_data
-    #     )
+        diffsky_data = lcmp.add_black_hole_quantities_to_diffsky_data(
+            lc_data, diffsky_data
+        )
 
-    #     patch_key, nfw_key = jran.split(patch_key, 2)
-    #     lc_data, diffsky_data = lcmp.reposition_satellites(
-    #         sim_info, lc_data, diffsky_data, nfw_key
-    #     )
+        patch_key, nfw_key = jran.split(patch_key, 2)
+        lc_data, diffsky_data = lcmp.reposition_satellites(
+            sim_info, lc_data, diffsky_data, nfw_key
+        )
 
-    #     bn_out = lcmp.LC_MOCK_BNPAT.format(stepnum, lc_patch)
-    #     fn_out = os.path.join(drn_out, bn_out)
-    #     lcmp.write_lc_dbk_sed_mock_to_disk(
-    #         fn_out, phot_info, lc_data, diffsky_data, filter_nicknames
-    #     )
-    #     metadata_sfh_mock.append_metadata(fn_out, sim_name, mock_version_name)
+        bn_out = lcmp.LC_MOCK_BNPAT.format(stepnum, lc_patch)
+        fn_out = os.path.join(drn_out, bn_out)
+        lcmp.write_lc_dbk_sed_mock_to_disk(
+            fn_out, phot_info, lc_data, diffsky_data, filter_nicknames
+        )
+        metadata_sfh_mock.append_metadata(fn_out, sim_name, mock_version_name)
 
-    #     del lc_data
-    #     del diffsky_data
-    #     gc.collect()
-    #     jax.clear_caches()
+        del lc_data
+        del diffsky_data
+        gc.collect()
+        jax.clear_caches()
 
-    # end_script = time()
-    # runtime = (end_script - start_script) / 60.0
+    end_script = time()
+    runtime = (end_script - start_script) / 60.0
 
-    # end_script = time()
-    # n_patches = len(lc_patch_list)
-    # runtime = (end_script - start_script) / 60.0
-    # msg = f"Total runtime for {n_patches} patches = {runtime:.1f} minutes"
-    # if rank == 0:
-    #     print(msg)
+    end_script = time()
+    n_patches = len(lc_patch_list)
+    runtime = (end_script - start_script) / 60.0
+    msg = f"Total runtime for {n_patches} patches = {runtime:.1f} minutes"
+    if rank == 0:
+        print(msg)
