263ba3777ac36ee4be136cb6e35a5e365536be26
Andrew Hearin <ahearin@anl.gov>
2025-11-15 09:59:32 -0600

    Make mocks in reverse order

--- scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
+++ scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
@@ -233,7 +233,7 @@ if __name__ == "__main__":
     fn_sizes = [os.path.getsize(fn) for fn in fn_lc_list]
 
     rank_assignments, __ = mpi_utils.distribute_files_by_size(fn_sizes, nranks)
-    fn_lc_list_for_rank = [fn_lc_list[i] for i in rank_assignments[rank]]
+    fn_lc_list_for_rank = [fn_lc_list[i] for i in rank_assignments[rank]][::-1]
 
     start_script = time()
     for fn_lc_diffsky in fn_lc_list_for_rank:
