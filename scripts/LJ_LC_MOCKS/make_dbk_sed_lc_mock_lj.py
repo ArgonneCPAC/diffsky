d5a42b2f76fdc3db49cba0ec059f3d5e3a9a7559
Andrew Hearin <ahearin@anl.gov>
2025-11-15 16:54:39 -0600

    batch_size = 10_000

--- scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
+++ scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     parser.add_argument("mock_nickname", help="Nickname of the mock")
 
     parser.add_argument(
-        "-batch_size", help="Size of photometry batches", type=int, default=100_000
+        "-batch_size", help="Size of photometry batches", type=int, default=10_000
     )
 
     parser.add_argument(
