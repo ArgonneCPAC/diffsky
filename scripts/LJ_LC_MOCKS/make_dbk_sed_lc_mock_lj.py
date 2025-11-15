c965881540a02130b1c2fbc51807964396a5516f
Andrew Hearin <ahearin@anl.gov>
2025-11-15 10:18:19 -0600

    Reorder again

--- scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
+++ scripts/LJ_LC_MOCKS/make_dbk_sed_lc_mock_lj.py
@@ -234,6 +234,7 @@ if __name__ == "__main__":
 
     rank_assignments, __ = mpi_utils.distribute_files_by_size(fn_sizes, nranks)
     fn_lc_list_for_rank = [fn_lc_list[i] for i in rank_assignments[rank]]
+    fn_lc_list_for_rank = fn_lc_list_for_rank[::-1]
 
     if rank == 0:
         print(fn_lc_list_for_rank)
