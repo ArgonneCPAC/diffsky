d08740c818fc44d7539110c5d2b1341293ba5bc3
Andrew Hearin <ahearin@anl.gov>
2025-11-19 15:45:38 -0600

    Clean up test_dbk_from_mock.py

--- diffsky/experimental/tests/test_dbk_from_mock.py
+++ diffsky/experimental/tests/test_dbk_from_mock.py
@@ -6,7 +6,7 @@ from dsps.cosmology import DEFAULT_COSMOLOGY
 from jax import random as jran
 
 from ...param_utils import diffsky_param_wrapper as dpw
-from .. import dbk_from_mock, lc_phot_kern
+from .. import dbk_from_mock
 from .. import mc_diffsky_disk_bulge_knot_seds as mc_dbk_sed
 from . import test_lc_phot_kern as tlcphk
 
@@ -46,14 +46,8 @@ def test_disk_bulge_knot_phot_from_mock():
         ssp_err_pop_params,
         DEFAULT_COSMOLOGY,
     )
-    # dbk_phot_info, _dk_args, _dk_res = mc_dbk_sed._mc_diffsky_disk_bulge_knot_phot_kern(
-    #     *mc_args
-    # )
-    dbk_phot_info = mc_dbk_sed._mc_diffsky_disk_bulge_knot_phot_kern(*mc_args)
 
-    logsm_obs, logssfr_obs = lc_phot_kern._get_sfh_info_at_t_obs(
-        lc_data.t_table, dbk_phot_info["sfh_table"], lc_data.t_obs
-    )
+    dbk_phot_info = mc_dbk_sed._mc_diffsky_disk_bulge_knot_phot_kern(*mc_args)
 
     mock_args = (
         lc_data.z_obs,
@@ -107,21 +101,8 @@ def test_disk_bulge_knot_phot_from_mock():
         dbk_phot_info["mc_sfh_type"],
         dbk_phot_info["fknot"],
     )
-    # dbk_phot_info_from_mock, _dk_args2, _dk_res2 = (
-    #     dbk_from_mock._disk_bulge_knot_phot_from_mock(*mock_args)
-    # )
-    dbk_phot_info_from_mock = dbk_from_mock._disk_bulge_knot_phot_from_mock(*mock_args)
-    # return (
-    #     _dk_args,
-    #     _dk_res,
-    #     _dk_args2,
-    #     _dk_res2,
-    #     lc_data,
-    #     dbk_phot_info,
-    #     dbk_phot_info_from_mock,
-    # )
 
-    # assert np.allclose(mk1, mk2, rtol=0.01)
+    dbk_phot_info_from_mock = dbk_from_mock._disk_bulge_knot_phot_from_mock(*mock_args)
 
     assert np.allclose(
         dbk_phot_info["obs_mags"], dbk_phot_info_from_mock["obs_mags"], rtol=1e-3
