82765ce58e7f0ae852dee132b942a5b346d7e56d
Andrew Hearin <ahearin@anl.gov>
2025-11-20 14:15:00 -0600

    Rework load_diffsky_param_collection

--- diffsky/param_utils/param_loader.py
+++ diffsky/param_utils/param_loader.py
@@ -1,22 +1,11 @@
 """"""
 
-from collections import namedtuple
-
-import numpy as np
-
+from ..data_loaders import io_utils as iou
 from . import diffsky_param_wrapper as dpw
 
 
 def load_diffsky_param_collection(fn):
-    diffsky_params_str_arr = np.load(fn)
-
-    param_names = dpw.get_flat_param_names()
-    DiffskyParams = namedtuple("DiffskyParams", param_names)
-
-    reloaded_params = DiffskyParams(
-        *[diffsky_params_str_arr[key] for key in DiffskyParams._fields]
-    )
-
-    param_collection = dpw.get_param_collection_from_flat_array(reloaded_params)
-
+    """"""
+    flat_diffsky_params = iou.load_namedtuple_from_hdf5(fn)
+    param_collection = dpw.get_param_collection_from_flat_array(flat_diffsky_params)
     return param_collection
--- diffsky/param_utils/tests/test_param_loader.py
+++ diffsky/param_utils/tests/test_param_loader.py
@@ -1,7 +1,10 @@
 """"""
 
+from collections import namedtuple
+
 import numpy as np
 
+from ...data_loaders import io_utils as iou
 from .. import diffsky_param_wrapper as dpw
 from .. import param_loader
 
@@ -11,14 +14,14 @@ def test_load_diffsky_param_collection():
         *dpw.DEFAULT_PARAM_COLLECTION
     )
     all_pnames = dpw.get_flat_param_names()
-    dtype = [
-        (name, np.array(val).dtype, np.array(val).shape)
-        for name, val in zip(all_pnames, all_params_flat)
-    ]
-    all_params_flat_structured = np.array([all_params_flat], dtype=dtype)
 
-    np.save("all_params_flat", all_params_flat_structured)
-    param_collection = param_loader.load_diffsky_param_collection("all_params_flat.npy")
+    Params = namedtuple("Params", all_pnames)
+    all_named_params = Params(*all_params_flat)
+
+    fn = "diffsky_unit_testing_params.hdf5"
+    iou.write_namedtuple_to_hdf5(all_named_params, fn)
+
+    param_collection = param_loader.load_diffsky_param_collection(fn)
     all_params_flat2 = dpw.unroll_param_collection_into_flat_array(*param_collection)
 
     assert np.allclose(all_params_flat, all_params_flat2, rtol=1e-5)
