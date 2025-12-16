a6ee392b1fb1d3851a0de538f97196eef21bce21
Andrew Hearin <ahearin@anl.gov>
2025-12-16 13:56:14 -0600

    Fix bug in ellipse_proj_kernels.py and add new unit tests that previously failed

--- diffsky/ellipsoidal_shapes/ellipse_proj_kernels.py
+++ diffsky/ellipsoidal_shapes/ellipse_proj_kernels.py
@@ -13,22 +13,12 @@ from jax import random as jran
 
 Ellipse2DParams = namedtuple(
     "Ellipse2DParams",
-    (
-        "beta",
-        "alpha",
-        "psi",
-        "ellipticity",
-        "e_beta",
-        "e_alpha",
-        "A",
-        "B",
-        "C",
-        "Delta",
-    ),
+    ("alpha", "beta", "psi", "ellipticity", "e_alpha", "e_beta", "A", "B", "C"),
 )
 
 
 def mc_mu_phi(n, ran_key):
+    """Monte Carlo realization of random projection angles mu, phi"""
     mu_key, phi_key = jran.split(ran_key, 2)
     mu_ran = jran.uniform(mu_key, minval=-1, maxval=1, shape=(n,))
     phi_ran = jran.uniform(phi_key, minval=0, maxval=2 * jnp.pi, shape=(n,))
@@ -36,7 +26,7 @@ def mc_mu_phi(n, ran_key):
 
 
 def mc_ellipsoid_params(r50, b_over_a, c_over_a, ran_key):
-    """"""
+    """Monte Carlo realization of 2d ellipse with random projection angles mu, phi"""
     mu_ran, phi_ran = mc_mu_phi(r50.size, ran_key)
     a = r50
     b = b_over_a * a
@@ -51,19 +41,27 @@ def compute_ellipse2d(a, b, c, mu, phi):
 
     Parameters
     ----------
-    A, B, C : arrays, shape (n, )
+    a, b, c : arrays, shape (n, )
+        Length of the major, intermediate, and minor axes, respectively
+            x-axis is the major axis (length a)
+            y-axis is the intermediate axis (length b)
+            z-axis is the minor axis (length c)
+
+    mu, phi : arrays, shape (n, )
+        Angles defining the line-of-sight projection
+        mu = cos(θ) pertains to the angle between the z-axis and line-of-sight direction
+        phi is the azimuthal angle around the z-axis
 
     Returns
     -------
     Ellipse2DParams : namedtuple of arrays of shape (n, )
-        fields: (beta, alpha, psi, e1, e2, ellipticity, A, B, C, Delta)
-            beta : semi-major axis (α<β)
-            alpha : semi-minor axis (α<β)
-            psi : angle in radians between semi-major axis β and x-axis
-            e_beta, e_alpha : xy-coordinates of semi-major and semi-minor axes
-            ellipticity : ε ≡ 1 - alpha/beta
+        fields: (alpha, beta, psi, e1, e2, ellipticity, A, B, C)
+            alpha : semi-major axis (β<α)
+            beta : semi-minor axis (β<α)
+            psi : angle in radians between semi-major axis α and x-axis
+            e_alpha, e_beta : xy-coordinates of semi-major and semi-minor axes
+            ellipticity : ε ≡ 1 - beta/alpha
             A, B, C : Coefficients of ellipse equation Ax² + Bxy + Cy² = 1
-            Delta : denominator of equations defining A, B, C
 
     Notes
     -----
@@ -77,7 +75,7 @@ def compute_ellipse2d(a, b, c, mu, phi):
     θ is the angle between the z-axis and line-of-sight direction
     φ is the azimuthal angle around the z-axis
 
-    α and β are defined by Eq 11:
+    β and α are defined by Eq 11:
         (X/α)^2 + (Y/β)^2 = 1
 
     A, B, and C are defined by Eq 8:
@@ -87,65 +85,53 @@ def compute_ellipse2d(a, b, c, mu, phi):
         {A, B, C, θ, Φ} ==> {α, β}
 
     First compute eigenvalues {λ_1, λ_2} from the trace and discriminant of Eq 12
-        λ_1 = 1 / α^2
-        λ_2 = 1 / β^2
+        λ_1 = 1 / β^2
+        λ_2 = 1 / α^2
 
-    Since λ_1 >= λ_2, then α<=β, so that α is semi-minor and β is semi-major
+    Since λ_1 >= λ_2, then β<=α, so that β is semi-minor and α is semi-major
 
-    e_beta : xy coordinates of β
     e_alpha : xy coordinates of α
+    e_beta : xy coordinates of β
 
     psi = 2ω is defined by the last line in Eq 12
         -π < psi < π
-        psi = 0 ==> semi-major axis β is aligned with x-axis, i.e., e_beta = (1, 0)
-        psi > 0 ==> Counter-clockwise rotation of x-axis needed to align with β
-        psi < 0 ==> Clockwise rotation of x-axis needed to align with β
+        psi = 0 ==> semi-major axis α is aligned with x-axis, i.e., e_alpha = (1, 0)
+        psi > 0 ==> Counter-clockwise rotation of x-axis needed to align with α
+        psi < 0 ==> Clockwise rotation of x-axis needed to align with α
 
-        psi = π/2 ==> semi-major axis β is aligned with y-axis, i.e., e_beta = (0, 1)
-        psi = -π/2 ==> semi-major axis β anti-aligned with y-axis, e_beta = (0, -1)
-        psi = +/-π ==> semi-major axis β is aligned with -x-axis, i.e., e_beta = (-1, 0)
+        psi = π/2 ==> semi-major axis α is aligned with y-axis, i.e., e_alpha = (0, 1)
+        psi = -π/2 ==> semi-major axis α anti-aligned with y-axis, e_alpha = (0, -1)
+        psi = +/-π ==> semi-major axis α is aligned with -x-axis, i.e., e_alpha = (-1, 0)
 
     """
-    A, B, C, Delta = _compute_2d_ellipse_params(a, b, c, mu, phi)
-    beta, alpha = _calculate_ellipse2d_axes(A, B, C)
-    ellipticity = 1.0 - alpha / beta
+    A, B, C = _compute_2d_ellipse_params(a, b, c, mu, phi)
+    alpha, beta = _calculate_ellipse2d_axes(A, B, C)
+    ellipticity = 1.0 - beta / alpha
     omega = _calculate_ellipse2d_omega(A, B, C)
     psi = 2.0 * omega
-    e_beta, e_alpha = _get_xy_coords_of_projected_semi_axes(psi)
-    ellipse2d = Ellipse2DParams(
-        beta, alpha, psi, ellipticity, e_beta, e_alpha, A, B, C, Delta
-    )
+    e_alpha, e_beta = _get_xy_coords_of_projected_semi_axes(psi)
+    ellipse2d = Ellipse2DParams(alpha, beta, psi, ellipticity, e_alpha, e_beta, A, B, C)
     return ellipse2d
 
 
 @jjit
 def _compute_2d_ellipse_params(a, b, c, mu, phi):
-    """
-    Compute ellipse coefficients {A, B, C, Δ} defined by Eq. 8:
-        Ax² + Bxy + Cy² = 1
-
-    """
-    # Precompute trig and define convenient notation
+    """See Equation 9 (fixes error in paper - simplified derivation uses z=0)"""
     cos_phi = jnp.cos(phi)
     sin_phi = jnp.sin(phi)
     cos2_phi = cos_phi**2
     sin2_phi = sin_phi**2
     mu2 = mu**2
     one_minus_mu2 = 1.0 - mu2
+    a2 = a * a
+    b2 = b * b
+    c2 = c * c
 
-    # Calculate Δ after some regrouping of terms in Eq 9
-    term1 = a**2 * (cos2_phi + sin2_phi * one_minus_mu2)
-    term2 = b**2 * mu2
-    term3 = c**2 * (sin2_phi + cos2_phi * one_minus_mu2)
-    Delta = term1 + term2 + term3
+    A = (mu2 * cos2_phi / a2) + (mu2 * sin2_phi / b2) + (one_minus_mu2 / c2)
+    C = (sin2_phi / a2) + (cos2_phi / b2)
+    B = 2 * mu * sin_phi * cos_phi * (1 / b2 - 1 / a2)
 
-    A = (c**2 * cos2_phi + a**2 * sin2_phi + b**2 * mu2) / Delta
-
-    B = 2 * (a**2 - c**2) * cos_phi * sin_phi * one_minus_mu2 / Delta
-
-    C = (c**2 * sin2_phi + a**2 * cos2_phi + b**2 * mu2) / Delta
-
-    return A, B, C, Delta
+    return A, B, C
 
 
 @jjit
@@ -165,16 +151,16 @@ def _calculate_ellipse2d_axes(A, B, C):
     lambda1 = 0.5 * (trace + sqrt_discriminant)  # Larger eigenvalue
     lambda2 = 0.5 * (trace - sqrt_discriminant)  # Smaller eigenvalue
 
-    msk_neg = lambda2 <= 0  # edge case where trace=sqrt_discriminant (α=β)
+    msk_neg = lambda2 <= 0  # edge case where trace=sqrt_discriminant (β=α)
     # set edge case to circle with unit length
     lambda1 = jnp.where(msk_neg, 1.0, lambda1)
     lambda2 = jnp.where(msk_neg, 1.0, lambda2)
 
-    # Calculate semi-axes α and β
-    beta = jnp.sqrt(1.0 / lambda2)  # Semi-major axis (from smaller eigenvalue)
-    alpha = jnp.sqrt(1.0 / lambda1)  # Semi-minor axis (from larger eigenvalue)
+    # Calculate semi-axes β and α
+    alpha = jnp.sqrt(1.0 / lambda2)  # Semi-major axis (from smaller eigenvalue)
+    beta = jnp.sqrt(1.0 / lambda1)  # Semi-minor axis (from larger eigenvalue)
 
-    return beta, alpha
+    return alpha, beta
 
 
 @jjit
@@ -186,13 +172,13 @@ def _calculate_ellipse2d_omega(A, B, C):
 
 @jjit
 def _get_xy_coords_of_projected_semi_axes(psi):
-    x_beta = jnp.cos(psi)
-    y_beta = jnp.sin(psi)
+    x_alpha = jnp.cos(psi)
+    y_alpha = jnp.sin(psi)
 
-    x_alpha = jnp.cos(psi + jnp.pi / 2)
-    y_alpha = jnp.sin(psi + jnp.pi / 2)
+    x_beta = jnp.cos(psi + jnp.pi / 2)
+    y_beta = jnp.sin(psi + jnp.pi / 2)
 
-    e_beta = jnp.array((x_beta, y_beta)).T
     e_alpha = jnp.array((x_alpha, y_alpha)).T
+    e_beta = jnp.array((x_beta, y_beta)).T
 
-    return e_beta, e_alpha
+    return e_alpha, e_beta
