0e4e60f6576d3be70dfd5658ae5964263269ac46
Andrew Hearin <ahearin@anl.gov>
2025-12-30 12:54:32 -0600

    Implement separate disk plotter

--- diffsky/ellipsoidal_shapes/diagnostics/tests/test_plot_b_over_a_rp13.py
+++ diffsky/ellipsoidal_shapes/diagnostics/tests/test_plot_b_over_a_rp13.py
@@ -4,4 +4,8 @@ from .. import plot_b_over_a_rp13 as pbsrp13
 
 
 def test_make_bulge_rp13_comparison_plot():
-    fig = pbsrp13.make_bulge_rp13_comparison_plot()
+    pbsrp13.make_bulge_rp13_comparison_plot()
+
+
+def test_make_disk_rp13_comparison_plot():
+    pbsrp13.make_disk_rp13_comparison_plot()
