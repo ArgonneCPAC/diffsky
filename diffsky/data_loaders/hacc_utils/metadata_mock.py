a759ce57b4e031779b494fb9c1b95df21626d4d4
Andrew Hearin <ahearin@anl.gov>
2026-01-02 10:44:50 -0600

    Implement astropy units as string dump

--- diffsky/data_loaders/hacc_utils/metadata_mock.py
+++ diffsky/data_loaders/hacc_utils/metadata_mock.py
@@ -28,306 +28,402 @@ def get_column_metadata(column_names=None):
 
 
 def get_metadata_all_columns():
+    try:
+        from astropy import units as u
+        from astropy.cosmology import units as cu
+
+    except ImportError:
+        raise ImportError("Must have astropy installed to attach units to metadata")
+
+    u.add_enabled_units(cu)
+    unit_mpch = u.Mpc / cu.littleh
 
     metadata_all_columns = dict()
-    metadata_all_columns["central"] = ("None", "0 for satellite, 1 for central")
-    metadata_all_columns["core_tag"] = ("None", "ID of the simulated core")
-    metadata_all_columns["ra"] = ("degrees", "right ascension")
-    metadata_all_columns["dec"] = ("degrees", "declination")
+    metadata_all_columns["central"] = (
+        str(u.dimensionless_unscaled),
+        "0 for satellite, 1 for central",
+    )
+    metadata_all_columns["core_tag"] = (
+        str(u.dimensionless_unscaled),
+        "ID of the simulated core",
+    )
+    metadata_all_columns["ra"] = (str(u.deg), "right ascension")
+    metadata_all_columns["dec"] = (str(u.deg), "declination")
 
     metadata_all_columns["stepnum"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Timestep of the simulation. Varies between 0 and 499 for HACC gravity-only N-body simulations.",
     )
-    metadata_all_columns["lc_patch"] = ("None", "Lightcone patch number")
+    metadata_all_columns["lc_patch"] = (
+        str(u.dimensionless_unscaled),
+        "Lightcone patch number",
+    )
 
     metadata_all_columns["early_index"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Diffmah parameter for halo mass assembly",
     )
     metadata_all_columns["late_index"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Diffmah parameter for halo mass assembly",
     )
-    metadata_all_columns["logm0"] = ("None", "Diffmah parameter for halo mass assembly")
-    metadata_all_columns["logtc"] = ("None", "Diffmah parameter for halo mass assembly")
+    metadata_all_columns["logm0"] = (
+        str(u.dimensionless_unscaled),
+        "Diffmah parameter for halo mass assembly",
+    )
+    metadata_all_columns["logtc"] = (
+        str(u.dimensionless_unscaled),
+        "Diffmah parameter for halo mass assembly",
+    )
     metadata_all_columns["t_peak"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Diffmah parameter for halo mass assembly",
     )
 
     metadata_all_columns["has_diffmah_fit"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "1 if core has a diffmah fit, 0 if not",
     )
 
-    metadata_all_columns["lgmcrit"] = ("None", "Diffstar parameter for galaxy SFH")
-    metadata_all_columns["lgy_at_mcrit"] = ("None", "Diffstar parameter for galaxy SFH")
-    metadata_all_columns["indx_lo"] = ("None", "Diffstar parameter for galaxy SFH")
-    metadata_all_columns["indx_hi"] = ("None", "Diffstar parameter for galaxy SFH")
-    metadata_all_columns["lg_qt"] = ("None", "Diffstar parameter for galaxy SFH")
-    metadata_all_columns["qlglgdt"] = ("None", "Diffstar parameter for galaxy SFH")
-    metadata_all_columns["lg_drop"] = ("None", "Diffstar parameter for galaxy SFH")
-    metadata_all_columns["lg_rejuv"] = ("None", "Diffstar parameter for galaxy SFH")
+    metadata_all_columns["lgmcrit"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
+    metadata_all_columns["lgy_at_mcrit"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
+    metadata_all_columns["indx_lo"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
+    metadata_all_columns["indx_hi"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
+    metadata_all_columns["lg_qt"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
+    metadata_all_columns["qlglgdt"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
+    metadata_all_columns["lg_drop"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
+    metadata_all_columns["lg_rejuv"] = (
+        str(u.dimensionless_unscaled),
+        "Diffstar parameter for galaxy SFH",
+    )
 
-    metadata_all_columns["logmp0"] = ("Msun (not Msun/h)", "Log10 of halo mass at z=0")
-    metadata_all_columns["logmp_obs"] = ("Msun (not Msun/h)", "Log10 of halo mass at z")
+    logMsunh = u.dex(u.Msun / cu.littleh)
+    logMsunhh = u.dex(u.Msun / cu.littleh / cu.littleh)
+
+    metadata_all_columns["logmp0"] = (str(logMsunh), "Log10 of halo mass at z=0")
+    metadata_all_columns["logmp_obs"] = (str(logMsunh), "Log10 of halo mass at z")
     metadata_all_columns["logmp_obs_host"] = (
-        "Msun (not Msun/h)",
+        str(logMsunh),
         "Log10 of mass of parent halo at z",
     )
     metadata_all_columns["logsm_obs"] = (
-        "Msun (not Msun/h/h)",
+        str(logMsunhh),
         "Log10 of stellar mass at z",
     )
     metadata_all_columns["logssfr_obs"] = (
-        "1/yr ",
+        str(u.dex(1 / u.yr)),
         "Log10(SFR/Mstar) at time of observation",
     )
 
-    metadata_all_columns["top_host_idx"] = ("None", "Index of host core")
+    metadata_all_columns["top_host_idx"] = (
+        str(u.dimensionless_unscaled),
+        "Index of host core",
+    )
 
-    metadata_all_columns["x"] = ("Mpc/h", "Comoving coordinate of lightcone position")
-    metadata_all_columns["y"] = ("Mpc/h", "Comoving coordinate of lightcone position")
-    metadata_all_columns["z"] = ("Mpc/h", "Comoving coordinate of lightcone position")
+    metadata_all_columns["x"] = (
+        str(unit_mpch),
+        "Comoving coordinate of lightcone position",
+    )
+    metadata_all_columns["y"] = (
+        str(unit_mpch),
+        "Comoving coordinate of lightcone position",
+    )
+    metadata_all_columns["z"] = (
+        str(unit_mpch),
+        "Comoving coordinate of lightcone position",
+    )
 
     metadata_all_columns["x_host"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "Comoving coord of host halo lightcone position",
     )
     metadata_all_columns["y_host"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "Comoving coord of host halo lightcone position",
     )
     metadata_all_columns["z_host"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "Comoving coord of host halo lightcone position",
     )
 
     metadata_all_columns["x_nfw"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "Comoving coord with NFW-repositioned satellites",
     )
     metadata_all_columns["y_nfw"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "Comoving coord with NFW-repositioned satellites",
     )
     metadata_all_columns["z_nfw"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "Comoving coord with NFW-repositioned satellites",
     )
 
-    metadata_all_columns["vx"] = ("km/s", "Comoving peculiar velocity in x-direction")
-    metadata_all_columns["vy"] = ("km/s", "Comoving peculiar velocity in y-direction")
-    metadata_all_columns["vz"] = ("km/s", "Comoving peculiar velocity in z-direction")
+    metadata_all_columns["vx"] = (
+        str(u.km / u.s),
+        "Comoving peculiar velocity in x-direction",
+    )
+    metadata_all_columns["vy"] = (
+        str(u.km / u.s),
+        "Comoving peculiar velocity in y-direction",
+    )
+    metadata_all_columns["vz"] = (
+        str(u.km / u.s),
+        "Comoving peculiar velocity in z-direction",
+    )
 
     metadata_all_columns["top_host_infall_fof_halo_eigS1X"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "x-component of first eigendirection of halo shape (unreduced intertia tensor)",
     )
     metadata_all_columns["top_host_infall_fof_halo_eigS1Y"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "y-component of first eigendirection of halo shape (unreduced intertia tensor)",
     )
     metadata_all_columns["top_host_infall_fof_halo_eigS1Z"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "z-component of first eigendirection of halo shape (unreduced intertia tensor)",
     )
 
     metadata_all_columns["top_host_infall_fof_halo_eigS2X"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "x-component of second eigendirection of halo shape (unreduced intertia tensor)",
     )
     metadata_all_columns["top_host_infall_fof_halo_eigS2Y"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "y-component of second eigendirection of halo shape (unreduced intertia tensor)",
     )
     metadata_all_columns["top_host_infall_fof_halo_eigS2Z"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "z-component of second eigendirection of halo shape (unreduced intertia tensor)",
     )
 
     metadata_all_columns["top_host_infall_fof_halo_eigS3X"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "x-component of third eigendirection of halo shape (unreduced intertia tensor)",
     )
     metadata_all_columns["top_host_infall_fof_halo_eigS3Y"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "y-component of third eigendirection of halo shape (unreduced intertia tensor)",
     )
     metadata_all_columns["top_host_infall_fof_halo_eigS3Z"] = (
-        "Mpc/h",
+        str(unit_mpch),
         "z-component of third eigendirection of halo shape (unreduced intertia tensor)",
     )
 
-    metadata_all_columns["redshift_true"] = ("None", "True redshift")
+    metadata_all_columns["redshift_true"] = (
+        str(u.dimensionless_unscaled),
+        "True redshift",
+    )
     metadata_all_columns["ra_nfw"] = (
-        "degrees",
+        str(u.deg),
         "Longitude in degrees. Based on NFW-repositioned satellites.",
     )
     metadata_all_columns["dec_nfw"] = (
-        "degrees",
+        str(u.deg),
         "Latitude in degrees. Based on NFW-repositioned satellites.",
     )
 
     metadata_all_columns["uran_av"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Array for adding noise to dust parameter Av",
     )
     metadata_all_columns["uran_delta"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Array for adding noise to dust parameter delta",
     )
     metadata_all_columns["uran_funo"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Array for adding noise to dust parameter funo",
     )
     metadata_all_columns["uran_pburst"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Array for adding noise to burstiness",
     )
 
     metadata_all_columns["delta_mag_ssp_scatter"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Array for adding noise to SSP SED errors",
     )
 
-    metadata_all_columns["av"] = ("None", "Dust attenuation curve parameter")
-    metadata_all_columns["delta"] = ("None", "Dust attenuation curve parameter")
-    metadata_all_columns["funo"] = ("None", "Dust attenuation curve parameter")
+    metadata_all_columns["av"] = (
+        str(u.dimensionless_unscaled),
+        "Dust attenuation curve parameter",
+    )
+    metadata_all_columns["delta"] = (
+        str(u.dimensionless_unscaled),
+        "Dust attenuation curve parameter",
+    )
+    metadata_all_columns["funo"] = (
+        str(u.dimensionless_unscaled),
+        "Dust attenuation curve parameter",
+    )
 
-    metadata_all_columns["bulge_to_total"] = ("None", "Bulge-to-total mass ratio")
-    metadata_all_columns["fbulge_tcrit"] = ("None", "Bulge model parameter")
-    metadata_all_columns["fbulge_early"] = ("None", "Bulge model parameter")
-    metadata_all_columns["fbulge_late"] = ("None", "Bulge model parameter")
+    metadata_all_columns["bulge_to_total"] = (
+        str(u.dimensionless_unscaled),
+        "Bulge-to-total mass ratio",
+    )
+    metadata_all_columns["fbulge_tcrit"] = (
+        str(u.dimensionless_unscaled),
+        "Bulge model parameter",
+    )
+    metadata_all_columns["fbulge_early"] = (
+        str(u.dimensionless_unscaled),
+        "Bulge model parameter",
+    )
+    metadata_all_columns["fbulge_late"] = (
+        str(u.dimensionless_unscaled),
+        "Bulge model parameter",
+    )
 
-    metadata_all_columns["black_hole_mass"] = ("Msun", "Black hole mass")
+    metadata_all_columns["black_hole_mass"] = (str(u.Msun), "Black hole mass")
     metadata_all_columns["black_hole_eddington_ratio"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "dimensionless Eddington ratio",
     )
     metadata_all_columns["black_hole_accretion_rate"] = (
-        "Msun/yr",
+        str(u.Msun / u.yr),
         "Black hole mass accretion rate",
     )
 
     metadata_all_columns["lgfburst"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Burstiness parameter. Specifies burst intensity.",
     )
     metadata_all_columns["lgyr_max"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Burstiness parameter. Specifies burst duration.",
     )
     metadata_all_columns["lgyr_peak"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Burstiness parameter. Specifies burst recency.",
     )
 
     metadata_all_columns["fknot"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Fraction of disk mass contained in star-forming knots",
     )
 
     metadata_all_columns["mc_sfh_type"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Boolean specifies the type of star formation history."
         "0 for quenched, 1 for smooth main sequence, 2 for bursty main sequence",
     )
 
-    metadata_all_columns["r50_disk"] = ("kpc", "Half-mass radius of disk")
-    metadata_all_columns["r50_bulge"] = ("kpc", "Half-mass radius of bulge")
+    metadata_all_columns["r50_disk"] = (str(u.kpc), "Half-mass radius of disk")
+    metadata_all_columns["r50_bulge"] = (str(u.kpc), "Half-mass radius of bulge")
     metadata_all_columns["zscore_r50_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Gaussian random used to add scatter to disk radius",
     )
     metadata_all_columns["zscore_r50_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Gaussian random used to add scatter to bulge radius",
     )
 
     metadata_all_columns["b_over_a_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "3d axis ratio b/a, where 0 < b/a < 1",
     )
     metadata_all_columns["c_over_a_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "3d axis ratio c/a, where 0 < c/a < 1",
     )
     metadata_all_columns["b_over_a_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "3d axis ratio b/a, where 0 < b/a < 1",
     )
     metadata_all_columns["c_over_a_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "3d axis ratio c/a, where 0 < c/a < 1",
     )
 
     metadata_all_columns["beta_disk"] = (
-        "kpc",
+        str(u.kpc),
         "2d projected size of the semi-major axis of the disk",
     )
     metadata_all_columns["alpha_disk"] = (
-        "kpc",
+        str(u.kpc),
         "2d projected size of the semi-minor axis of the disk",
     )
     metadata_all_columns["ellipticity_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "2d projected size of the semi-major axis of the disk",
     )
     metadata_all_columns["psi_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Angular coordinate of projected semi-major axis of the disk, where 0<ψ<2π",
     )
 
     metadata_all_columns["beta_bulge"] = (
-        "kpc",
+        str(u.kpc),
         "2d projected size of the semi-major axis of the bulge",
     )
     metadata_all_columns["alpha_bulge"] = (
-        "kpc",
+        str(u.kpc),
         "2d projected size of the semi-minor axis of the bulge",
     )
     metadata_all_columns["ellipticity_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "2d projected size of the semi-major axis of the bulge",
     )
     metadata_all_columns["psi_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "Angular coordinate of projected semi-major axis of the bulge, where 0<ψ<2π",
     )
 
     metadata_all_columns["e_beta_x_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "x-coord of unit-normalized semi-major axis of the disk",
     )
     metadata_all_columns["e_beta_y_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "y-coord of unit-normalized semi-major axis of the disk",
     )
     metadata_all_columns["e_alpha_x_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "x-coord of unit-normalized semi-minor axis of the disk",
     )
     metadata_all_columns["e_alpha_y_disk"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "y-coord of unit-normalized semi-minor axis of the disk",
     )
 
     metadata_all_columns["e_beta_x_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "x-coord of unit-normalized semi-major axis of the bulge",
     )
     metadata_all_columns["e_beta_y_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "y-coord of unit-normalized semi-major axis of the bulge",
     )
     metadata_all_columns["e_alpha_x_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "x-coord of unit-normalized semi-minor axis of the bulge",
     )
     metadata_all_columns["e_alpha_y_bulge"] = (
-        "None",
+        str(u.dimensionless_unscaled),
         "y-coord of unit-normalized semi-minor axis of the bulge",
     )
 
@@ -392,7 +488,7 @@ def append_metadata(fnout, sim_name, mock_version_name, z_phot_table, filter_nic
             key_out = "data/" + nickname
             assert key_out in hdf_out.keys(), f"{key_out} is missing from {fnout}"
 
-            hdf_out[key_out].attrs["unit"] = "None"
+            hdf_out[key_out].attrs["unit"] = ""
             hdf_out[key_out].attrs["description"] = COMPOSITE_MAG_MSG
 
             # Component magnitudes
@@ -401,7 +497,7 @@ def append_metadata(fnout, sim_name, mock_version_name, z_phot_table, filter_nic
                 assert key_out in hdf_out.keys(), f"{key_out} is missing from {fnout}"
 
                 msg = COMPONENT_MAG_MSG_PAT.format(component)
-                hdf_out[key_out].attrs["unit"] = "None"
+                hdf_out[key_out].attrs["unit"] = ""
                 hdf_out[key_out].attrs["description"] = msg
 
 
--- diffsky/data_loaders/hacc_utils/tests/test_metadata_mock.py
+++ diffsky/data_loaders/hacc_utils/tests/test_metadata_mock.py
@@ -1,9 +1,29 @@
 """"""
 
+import pytest
+
 from .. import metadata_mock
 
+try:
+    from astropy import units as u
 
+    HAS_ASTROPY = True
+except ImportError:
+    HAS_ASTROPY = False
+ASTROPY_MSG = "Must have astropy installed to run tests of metadata_mock"
+
+
+@pytest.mark.skipif(not HAS_ASTROPY, reason=ASTROPY_MSG)
 def test_get_column_metadata():
 
     column_metadata = metadata_mock.get_column_metadata()
     assert len(column_metadata) > 0
+    for colname, metadata in column_metadata.items():
+        unit_string, description = metadata
+        assert len(description) > 0
+        try:
+            u.Unit(unit_string)
+        except ValueError:
+            raise ValueError(
+                f"`{colname}` has unrecognized unit string = `{unit_string}`"
+            )
