a5af4cc2df931474ad08b15dba40c559b0d7dd7e
Andrew Hearin <ahearin@anl.gov>
2026-01-22 11:24:26 -0600

    update lc_mock checker

--- scripts/LJ_LC_MOCKS/check_lc_mock_completeness.py
+++ scripts/LJ_LC_MOCKS/check_lc_mock_completeness.py
@@ -4,7 +4,17 @@ import argparse
 import os
 from glob import glob
 
+import numpy as np
+
 BN_PAT = "lc_cores-{0}.{1}.*.hdf5"
+
+
+def get_stepnum_and_patch(bname):
+    _stepnum, _patch = bname.split("-")[1].split(".")[:2]
+    stepnum = int(_stepnum)
+    patch = int(_patch)
+    return stepnum, patch
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,3 +29,7 @@ if __name__ == "__main__":
 
     fn_pat = os.path.join(drn, BN_PAT.format("*", "*"))
     fn_list = glob(fn_pat)
+    bn_list = [os.path.basename(fn) for fn in fn_list]
+
+    step_collector = np.unique([get_stepnum_and_patch(bn)[0] for bn in bn_list])
+    patch_collector = np.unique([get_stepnum_and_patch(bn)[1] for bn in bn_list])
