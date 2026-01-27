ead2009e1f061358eeb35889614c5049af0cd32e
Patrick Wells <patrick.wells.95@gmail.com>
2026-01-27 08:28:03 -0600

    Basic opencosmo compute routine

---
+++ diffsky/data_loaders/opencosmo_utils/compute.py
@@ -0,0 +1,150 @@
+from collections import namedtuple
+
+import numpy as np
+import opencosmo as oc
+from diffmah import DiffmahParams
+from diffstar import DiffstarParams
+from dsps.cosmology import age_at_z
+
+from diffsky import phot_utils
+from diffsky.experimental import dbk_phot_from_mock
+from diffsky.experimental import precompute_ssp_phot as psspp
+
+
+def compute_phot_from_diffsky_mocks(
+    catalog: oc.Lightcone,
+    aux_data: dict,
+    survey_name: str = "lsst",
+    bands: list[str] = ["u", "g", "r", "i", "z", "y"],
+    insert: bool = True,
+):
+    band_names = set(map(lambda band: f"{survey_name}_{band}", bands))
+    if "tcurves" not in aux_data:
+        raise ValueError("Missing transmission curves in auxiliary data!")
+
+    known_tcurves = aux_data["tcurves"]._fields
+    missing_tcurves = band_names.difference(known_tcurves)
+    if missing_tcurves:
+        raise ValueError(f"Missing transmission curves for bands {missing_tcurves}")
+
+    Tcurves = namedtuple("Tcurves", tuple(band_names))
+    data = {bn: getattr(aux_data["tcurves"], bn) for bn in band_names}
+    tcurves = Tcurves(**data)
+
+    z_range = catalog.z_range
+    z_phot_table = np.linspace(*z_range, 10)
+
+    wave_eff_table = phot_utils.get_wave_eff_table(z_phot_table, tcurves)
+    cosmology_parameters = prep_cosmology_parameters(catalog.cosmology)
+    precomputed_ssp_mag_table = psspp.get_precompute_ssp_mag_redshift_table(
+        tcurves, aux_data["ssp_data"], z_phot_table, cosmology_parameters
+    )
+    catalog = catalog.evaluate(
+        age_at_z_, vectorize=True, cosmology=cosmology_parameters
+    )
+
+    return catalog.evaluate(
+        compute_photometry_managed,
+        cosmology=cosmology_parameters,
+        ssp_data=aux_data["ssp_data"],
+        precomputed_ssp_mag_table=precomputed_ssp_mag_table,
+        wave_eff_table=wave_eff_table,
+        param_collection=aux_data["param_collection"],
+        z_phot_table=z_phot_table,
+        insert=insert,
+        Ob0=catalog.cosmology.Ob0,
+        vectorize=True,
+    )
+
+
+def prep_cosmology_parameters(cosmology):
+    try:
+        w0 = cosmology.wo
+        wa = cosmology.wa
+    except AttributeError:  # Why astropy... Why...
+        w0 = -1
+        wa = 0
+    Cosmology_t = namedtuple("Cosmology_t", ("Om0", "w0", "wa", "h"))
+    return Cosmology_t(cosmology.Om0, w0, wa, cosmology.h)
+
+
+def age_at_z_(redshift, cosmology):
+    return {
+        "t_obs": np.array(
+            age_at_z(redshift, cosmology.Om0, cosmology.w0, cosmology.wa, cosmology.h)
+        )
+    }
+
+
+def compute_photometry_managed(
+    logm0,
+    logtc,
+    early_index,
+    late_index,
+    t_peak,  # diffmah params
+    lgmcrit,
+    lgy_at_mcrit,
+    indx_lo,
+    indx_hi,  # ms params
+    lg_qt,
+    qlglgdt,
+    lg_drop,
+    lg_rejuv,  # Q params
+    uran_av,
+    uran_delta,
+    uran_funo,
+    uran_pburst,
+    delta_mag_ssp_scatter,  # ¯\_(ツ)_/¯ (I am not a real astrophysicist)
+    redshift,
+    t_obs,
+    mc_sfh_type,
+    ssp_data,
+    precomputed_ssp_mag_table,
+    z_phot_table,
+    wave_eff_table,
+    param_collection,
+    cosmology,
+    Ob0,
+):
+    mc_is_q = mc_sfh_type == 0
+    mah_params = DiffmahParams(
+        logm0=logm0,
+        logtc=logtc,
+        early_index=early_index,
+        late_index=late_index,
+        t_peak=t_peak,
+    )
+    sfh_params = DiffstarParams(
+        lgmcrit=lgmcrit,
+        lgy_at_mcrit=lgy_at_mcrit,
+        indx_lo=indx_lo,
+        indx_hi=indx_hi,
+        lg_qt=lg_qt,
+        qlglgdt=qlglgdt,
+        lg_drop=lg_drop,
+        lg_rejuv=lg_rejuv,
+    )
+
+    result = dbk_phot_from_mock._reproduce_mock_phot_kern(
+        mc_is_q,
+        uran_av,
+        uran_delta,
+        uran_funo,
+        uran_pburst,
+        delta_mag_ssp_scatter,
+        sfh_params,
+        redshift,
+        t_obs,
+        mah_params,
+        ssp_data,
+        precomputed_ssp_mag_table,
+        z_phot_table,
+        wave_eff_table,
+        param_collection.mzr_params,
+        param_collection.spspop_params,
+        param_collection.scatter_params,
+        param_collection.ssperr_params,
+        cosmology,
+        Ob0 / cosmology.Om0,
+    )[0]
+    return result.obs_mags
--- diffsky/data_loaders/opencosmo_utils/test/test_load.py
+++ diffsky/data_loaders/opencosmo_utils/test/test_load.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import opencosmo as oc
 import pytest
 
+from diffsky.data_loaders.opencosmo_utils.compute import compute_phot_from_diffsky_mocks
 from diffsky.data_loaders.opencosmo_utils.load import load_diffsky_mock
 
 
@@ -21,3 +22,8 @@ def test_load(test_data_dir):
 
     assert isinstance(aux_data, dict)
     assert isinstance(aux_data_synths, dict)
+
+
+def test_compute(test_data_dir):
+    catalog, aux_data = load_diffsky_mock(test_data_dir)
+    results = compute_phot_from_diffsky_mocks(catalog, aux_data, insert=False)
