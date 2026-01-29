e8c793c080ff6c882f5ad876667ef1446e7a9092
Patrick Wells <patrick.wells.95@gmail.com>
2026-01-29 10:59:52 -0600

    Update interface naming, add example notebook

--- diffsky/data_loaders/opencosmo_utils/__init__.py
+++ diffsky/data_loaders/opencosmo_utils/__init__.py
@@ -7,18 +7,18 @@ except ImportError:
     )
 
 from .compute import (
-    compute_dbk_phot_from_diffsky_mocks,
-    compute_dbk_seds_from_diffsky_mocks,
-    compute_phot_from_diffsky_mocks,
-    compute_seds_from_diffsky_mocks,
+    compute_dbk_phot_from_diffsky_mock,
+    compute_dbk_seds_from_diffsky_mock,
+    compute_phot_from_diffsky_mock,
+    compute_seds_from_diffsky_mock,
 )
 from .load import add_transmission_curves, load_diffsky_mock
 
 __all__ = [
     "load_diffsky_mock",
-    "compute_dbk_phot_from_diffsky_mocks",
-    "compute_dbk_seds_from_diffsky_mocks",
-    "compute_phot_from_diffsky_mocks",
-    "compute_seds_from_diffsky_mocks",
+    "compute_dbk_phot_from_diffsky_mock",
+    "compute_dbk_seds_from_diffsky_mock",
+    "compute_phot_from_diffsky_mock",
+    "compute_seds_from_diffsky_mock",
     "add_transmission_curves",
 ]
