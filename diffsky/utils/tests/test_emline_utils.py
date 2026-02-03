7336f39877dd1081b4abca064cd4345a2629e3a1
Andrew Hearin <ahearin@anl.gov>
2026-02-03 11:35:20 -0600

    Fix bug in unit test test_fake_lineflux_table_cgs

--- diffsky/utils/tests/test_emline_utils.py
+++ diffsky/utils/tests/test_emline_utils.py
@@ -20,9 +20,9 @@ def test_lsun_cgs_units():
     assert np.allclose(emline_utils.L_SUN_CGS, u.Lsun.to(u.erg / u.s), rtol=1e-4)
 
 
-def test_fake_lineflux_table():
+def test_fake_lineflux_table_cgs():
     ssp_data = load_fake_ssp_data()
-    lineflux_table = emline_utils.fake_lineflux_table(
+    lineflux_table = emline_utils.fake_lineflux_table_cgs(
         ssp_data.ssp_lgmet, ssp_data.ssp_lg_age_gyr
     )
     assert np.all(np.isfinite(lineflux_table))
@@ -30,5 +30,10 @@ def test_fake_lineflux_table():
     # The youngest stars should have non-zero line flux
     assert np.all(lineflux_table[:, 0] > 0)
 
+    # The youngest stars should have reasonable values for the flux
+    assert np.all(lineflux_table[:, 0] > 1e30)
+    assert np.all(lineflux_table[:, 0] < 1e45)
+
     # The oldest stars should have zero line flux
-    assert np.allclose(lineflux_table[:, -1], 0.0)
+    lineflux_table_lsun = lineflux_table / emline_utils.L_SUN_CGS
+    assert np.allclose(lineflux_table_lsun[:, -1], 0.0, atol=1e-10)
