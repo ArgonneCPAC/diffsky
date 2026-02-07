eccce64abaa6f0a07e79bf35cf40e3df3d895241
Andrew Hearin <ahearin@anl.gov>
2026-02-07 10:49:07 -0600

    top-level import from diffsky.utils of _sig_slope

--- diffsky/utils/__init__.py
+++ diffsky/utils/__init__.py
@@ -4,4 +4,4 @@
 
 from .crossmatch_utils import crossmatch
 from .tw_utils import _tw_interp_kern, _tw_sig_slope, _tw_sigmoid
-from .utility_funcs import _inverse_sigmoid, _sigmoid
+from .utility_funcs import _inverse_sigmoid, _sig_slope, _sigmoid
