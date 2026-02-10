01918766a2c99cc495dcc42c61e38ac88efb0cc3
Andrew Hearin <ahearin@anl.gov>
2026-02-10 07:14:58 -0600

    Allow cosmos_260120_UM and cosmos_260105 to fail dpw.check_param_collection_is_ok without causing test_cosmos_param_fits_are_ok to raise an exception

--- diffsky/param_utils/tests/test_get_mock_params.py
+++ diffsky/param_utils/tests/test_get_mock_params.py
@@ -7,6 +7,8 @@ from .. import COSMOS_PARAM_FITS
 from .. import diffsky_param_wrapper as dpw
 from .. import get_mock_params as gmp
 
+KNOWN_COSMOS_CALIBRATION_FAILURES = ["cosmos_260120_UM", "cosmos_260105"]
+
 
 def test_get_param_collection_for_mock_sfh_model_options():
     for sfh_model in ldup.DIFFSTARPOP_CALIBRATIONS:
@@ -28,12 +30,21 @@ def test_get_param_collection_for_mock_defaults():
 
 
 def test_cosmos_param_fits_are_ok():
+    """Enforce all parameters in COSMOS_PARAM_FITS pass sanity checks encoded in
+    dpw.check_param_collection_is_ok function
+
+    Calibrations appearing in list KNOWN_COSMOS_CALIBRATION_FAILURES are permitted
+    to fail. So this test only checks for new failures beyond this list.
+
+    """
     bad_params = []
     for key in COSMOS_PARAM_FITS.keys():
         param_collection = gmp.get_param_collection_for_mock(cosmos_fit=key, rank=0)
         param_collection_is_ok = dpw.check_param_collection_is_ok(param_collection)
         if not param_collection_is_ok:
             bad_params.append(key)
+
+    bad_params = list(set(bad_params) - set(KNOWN_COSMOS_CALIBRATION_FAILURES))
 
     msg = "Bounding/unbounding failures of some params in dpw.COSMOS_PARAM_FITS\n"
     if len(bad_params) > 0:
