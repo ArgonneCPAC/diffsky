2ec4f6ac1d63450da71f30df566955bbaa1b4576
Andrew Hearin <ahearin@anl.gov>
2026-03-25 17:06:00 -0500

    Add simple model of _frac_disk_dom_kern

--- diffsky/experimental/disk_bulge_modeling/dbpop.py
+++ diffsky/experimental/disk_bulge_modeling/dbpop.py
@@ -1,7 +1,23 @@
 """"""
 
+from jax import jit as jjit
+from jax import numpy as jnp
+
 from ...utils import _sigmoid
 
+FDD_MIN = 0.1
+FDD_MAX = 0.9
 
-def frac_disk_dominated(logsm):
-    return _sigmoid(logsm, 10.75, 1.5, 0.9, 0.1)
+
+@jjit
+def _frac_disk_dom_kern(logsm, logssfr):
+    delta_fdd = _delta_fdd_ssfr(logssfr)
+    ylo = jnp.clip(0.9 + delta_fdd, min=FDD_MIN, max=FDD_MAX)
+    yhi = jnp.clip(0.1 + delta_fdd, min=FDD_MIN, max=FDD_MAX)
+    return _sigmoid(logsm, 10.75, 1.5, ylo, yhi)
+
+
+@jjit
+def _delta_fdd_ssfr(logssfr):
+    delta_fdd = _sigmoid(logssfr, -10.5, 2.0, -0.25, 0.25)
+    return delta_fdd
--- diffsky/experimental/disk_bulge_modeling/tests/test_dbpop.py
+++ diffsky/experimental/disk_bulge_modeling/tests/test_dbpop.py
@@ -1,13 +1,20 @@
 """"""
 
 import numpy as np
+from jax import random as jran
 
 from .. import dbpop
 
 
-def test_frac_disk_dominated():
-    logsm = np.linspace(6, 12, 100)
-    fdd = dbpop.frac_disk_dominated(logsm)
-    assert fdd.shape == logsm.shape
-    assert np.all(fdd >= 0)
-    assert np.all(fdd <= 1)
+def test_frac_disk_dom_kern():
+    ngals = 100
+    logsm = np.linspace(6, 12, ngals)
+    ZZ = np.zeros_like(logsm)
+
+    for logssfr in (-12, -11, -10, -9, -8):
+        fdd = dbpop._frac_disk_dom_kern(logsm, logssfr + ZZ)
+        assert fdd.shape == logsm.shape
+        assert np.all(fdd >= dbpop.FDD_MIN)
+        assert np.all(fdd <= dbpop.FDD_MAX)
+        assert np.all(np.diff(fdd) <= 0)
+        assert np.any(np.diff(fdd) < 0)
