97e860c3d5fde51a4da7253838dd2e5b88e94b08
Andrew Hearin <ahearin@anl.gov>
2026-03-25 18:09:44 -0500

    Implement get_fbulge_tcrit

--- diffsky/experimental/disk_bulge_modeling/dbpop.py
+++ diffsky/experimental/disk_bulge_modeling/dbpop.py
@@ -1,8 +1,10 @@
 """"""
 
+from diffstar.utils import cumulative_mstar_formed_galpop
 from jax import jit as jjit
 from jax import numpy as jnp
 from jax import random as jran
+from jax import vmap
 
 from ...utils import _sigmoid
 from . import disk_bulge_kernels as dbk
@@ -11,10 +13,14 @@ FDD_MIN = 0.1
 FDD_MAX = 0.9
 
 TCRIT_FRAC = 0.25
-FBULGE_EARLY_DD = 0.1
-FBULGE_LATE_DD = 0.3
+FBULGE_EARLY_DD = 0.3
+FBULGE_LATE_DD = 0.1
 FBULGE_EARLY_BD = 0.9
 FBULGE_LATE_BD = 0.7
+
+
+interp_vmap = jjit(vmap(jnp.interp, in_axes=(0, None, 0)))
+interp_vmap2 = jjit(vmap(jnp.interp, in_axes=(0, 0, None)))
 
 
 @jjit
@@ -44,3 +50,13 @@ def mc_fbulge_params(ran_key, logsm, logssfr, t10, t90):
         (fbulge_tcrit, fbulge_early, fbulge_late)
     )
     return fbulge_params
+
+
+@jjit
+def get_fbulge_tcrit(tarr, sfh_pop, t_obs):
+    smh_pop = cumulative_mstar_formed_galpop(tarr, sfh_pop)
+    logsmh_pop = jnp.log10(smh_pop)
+    logsm_obs = interp_vmap(t_obs, tarr, logsmh_pop)
+    logsmh_pop_clipped = jnp.clip(logsmh_pop, max=logsm_obs.reshape((-1, 1)))
+    fbulge_tcrit = dbk.calc_tform_pop(tarr, 10**logsmh_pop_clipped, 0.25)
+    return fbulge_tcrit, logsm_obs
