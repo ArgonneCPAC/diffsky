7ec0aa53e62b73e8bbb1841ebdaec43d25b4e9b7
Andrew Hearin <ahearin@anl.gov>
2026-04-03 16:15:35 -0500

    Add new test of check_param_collection_is_ok of merging model best-fit calibration

--- diffsky/param_utils/tests/test_get_mock_params.py
+++ diffsky/param_utils/tests/test_get_mock_params.py
@@ -3,8 +3,9 @@
 from ...experimental.sfh_model_calibrations import (
     load_diffsky_sfh_model_calibrations as ldup,
 )
-from .. import COSMOS_PARAM_FITS
+from .. import COSMOS_PARAM_FITS, COSMOS_PARAM_FITS_MERGING
 from .. import diffsky_param_wrapper as dpw
+from .. import diffsky_param_wrapper_merging as dpwm
 from .. import get_mock_params as gmp
 
 KNOWN_COSMOS_CALIBRATION_FAILURES = ["cosmos_260120_UM", "cosmos_260105"]
@@ -46,7 +47,31 @@ def test_cosmos_param_fits_are_ok():
 
     bad_params = list(set(bad_params) - set(KNOWN_COSMOS_CALIBRATION_FAILURES))
 
-    msg = "Bounding/unbounding failures of some params in dpw.COSMOS_PARAM_FITS\n"
+    msg = "Bounding/unbounding failures in dpw.COSMOS_PARAM_FITS\n"
+    if len(bad_params) > 0:
+        print(msg)
+        for bad_param in bad_params:
+            print(f"{bad_param} fails")
+        assert param_collection_is_ok, f"{key} fails check_param_collection_is_ok"
+
+
+def test_cosmos_param_fits_merging_are_ok():
+    """Enforce all parameters in COSMOS_PARAM_FITS_MERGING pass sanity checks encoded in
+    dpw.check_param_collection_is_ok function
+
+    Calibrations appearing in list KNOWN_COSMOS_CALIBRATION_FAILURES are permitted
+    to fail. So this test only checks for new failures beyond this list.
+
+    """
+    bad_params = []
+    for key, param_collection in COSMOS_PARAM_FITS_MERGING.items():
+        param_collection_is_ok = dpwm.check_param_collection_is_ok(param_collection)
+        if not param_collection_is_ok:
+            bad_params.append(key)
+
+    bad_params = list(set(bad_params) - set(KNOWN_COSMOS_CALIBRATION_FAILURES))
+
+    msg = "Bounding/unbounding failures in dpwm.COSMOS_PARAM_FITS_MERGING\n"
     if len(bad_params) > 0:
         print(msg)
         for bad_param in bad_params:
