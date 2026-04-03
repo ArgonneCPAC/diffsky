99aa4cdb1eec82edef08cf30c5a8b8affd076d96
Andrew Hearin <ahearin@anl.gov>
2026-04-03 16:14:53 -0500

    Add new COSMOS_PARAM_FITS_MERGING to top-level import from param_utils

--- diffsky/param_utils/__init__.py
+++ diffsky/param_utils/__init__.py
@@ -1,4 +1,4 @@
 """"""
 
-from .cosmos_calibrations import COSMOS_PARAM_FITS  # noqa
+from .cosmos_calibrations import COSMOS_PARAM_FITS, COSMOS_PARAM_FITS_MERGING  # noqa
 from .get_mock_params import get_param_collection_for_mock  # noqa
