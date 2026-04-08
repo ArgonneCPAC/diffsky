fe4bc4371ff6640edd666914b9bb30bd113529f4
Andrew Hearin <ahearin@anl.gov>
2026-04-08 14:00:38 -0500

    Update diffsky.data_loaders.load_ssp_data to be the same function as dsps.data_loaders.load_ssp_data. Update diffsky DEFAULT_SSP_BNAME=='fsps_v0.4.7_mist_c3k_a_kroupa_wNE_logGasU-2.0_logGasZ0.0.h5'

--- diffsky/data_loaders/load_ssp_data.py
+++ diffsky/data_loaders/load_ssp_data.py
@@ -1,71 +1,24 @@
 """"""
 
-import os
 import random
 import string
-from collections import OrderedDict, namedtuple
+from collections import namedtuple
 
 import h5py
 import numpy as np
-from dsps.data_loaders.defaults import DEFAULT_SSP_KEYS
+from dsps.data_loaders import load_ssp_templates as load_ssp_templates_dsps
 from dsps.data_loaders.retrieve_fake_fsps_data import (
     load_fake_ssp_data as load_fake_ssp_data_dsps,
 )
 
 from ..utils.emline_utils import L_SUN_CGS
+from .defaults import DEFAULT_SSP_BNAME
 
 EmissionLine = namedtuple("EmissionLine", ["line_wave", "line_flux"])
 
-DEFAULT_DIFFSKY_SSP_BNAME = "ssp_data_fsps_v3.2_emlines.hdf5"
 
-
-def load_ssp_templates(fn=None, drn=None, bn=DEFAULT_DIFFSKY_SSP_BNAME):
-    """Load SSP templates, optionally including emission lines if present
-
-    For emission lines, note that line_flux is stored on disk in units of Lsun/Msun.
-    But load_ssp_templates converts to cgs units after reading from disk.
-    And write_ssp_templates_to_disk converts back to Lsun/Msun before writing to disk.
-
-    """
-
-    if fn is None:
-        if drn is None:
-            try:
-                drn = os.environ["DSPS_DRN"]
-            except KeyError:
-                msg = (
-                    "Since you did not pass the fn or drn argument\n"
-                    "then you must have the DSPS_DRN environment variable set"
-                )
-                raise ValueError(msg)
-
-        fn = os.path.join(drn, bn)
-
-    msg = "{0} does not exist".format(fn)
-    assert os.path.isfile(fn), msg
-
-    EmissionLine = namedtuple("EmissionLine", ["line_wave", "line_flux"])
-
-    ssp_data_dict = OrderedDict()
-
-    with h5py.File(fn, "r") as hdf:
-        for key in DEFAULT_SSP_KEYS:
-            ssp_data_dict[key] = hdf[key][...]
-
-        if "emlines" in hdf.keys():
-            emlines_dict = OrderedDict()
-            for emline in hdf["emlines"].keys():
-                wave = float(hdf["emlines"][emline]["line_wave"][...])
-                flux = hdf["emlines"][emline]["line_flux"][...] * L_SUN_CGS
-                emlines_dict[emline] = EmissionLine(wave, flux)
-
-            EmissionLines = namedtuple("EmissionLines", list(emlines_dict.keys()))
-            emission_lines = EmissionLines(**emlines_dict)
-            ssp_data_dict["emlines"] = emission_lines
-
-    SSPData = namedtuple("SSPData", list(ssp_data_dict.keys()))
-    ssp_data = SSPData(**ssp_data_dict)
-
+def load_ssp_templates(fn=None, drn=None, bn=DEFAULT_SSP_BNAME):
+    ssp_data = load_ssp_templates_dsps(fn=fn, drn=drn, bn=bn)
     return ssp_data
 
 
@@ -79,19 +32,12 @@ def write_ssp_templates_to_disk(fn, ssp_data):
     """
 
     with h5py.File(fn, "w") as hdf_out:
-        for name, arr in zip(ssp_data._fields, ssp_data):
-            if name != "emlines":
-                hdf_out[name] = arr
+        for field, val in zip(ssp_data._fields, ssp_data):
+            if val is not None:
+                hdf_out[field] = val
 
-        if "emlines" in ssp_data._fields:
-            grp = hdf_out.create_group("emlines")
-
-            # Store each line's wavelength and flux table
-            gen = zip(ssp_data.emlines._fields, ssp_data.emlines)
-            for line_name, emline in gen:
-                line_grp = grp.create_group(line_name)
-                line_grp["line_wave"] = emline.line_wave
-                line_grp["line_flux"] = emline.line_flux / L_SUN_CGS
+        if "ssp_emline_wave" in ssp_data._fields:
+            hdf_out["ssp_emline_name"] = list(ssp_data.ssp_emline_wave._fields)
 
 
 def load_fake_ssp_data(n_lines=3, emline_names=None):
