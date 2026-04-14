33cd44f78c29f59eaf45cace5a511248b5d20dcb
Andrew Hearin <ahearin@anl.gov>
2026-04-14 11:53:57 -0500

    Use data dataset within make_lj_mock.py for loading hdf5 data

--- scripts/LJ_LC_MOCKS/make_lj_mock.py
+++ scripts/LJ_LC_MOCKS/make_lj_mock.py
@@ -240,9 +240,9 @@ if __name__ == "__main__":
 
         if synthetic_cores == 0:
             with h5py.File(fn_lc_cores, "r") as hdf:
-                nhalos_estimate = hdf["core_tag"].shape[0]
-                z_min_shell = 1.0 / hdf["scale_factor"][:].max() - 1
-                z_max_shell = 1.0 / hdf["scale_factor"][:].min() - 1
+                nhalos_estimate = hdf["data"]["core_tag"].shape[0]
+                z_min_shell = 1.0 / hdf["data"]["scale_factor"][:].max() - 1
+                z_max_shell = 1.0 / hdf["data"]["scale_factor"][:].min() - 1
         elif synthetic_cores == 1:
             mean_nhalos = mclh.estimate_nhalos_in_lightcone(
                 lgmp_min,
