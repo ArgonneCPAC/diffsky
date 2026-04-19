4fcdb4da91fc4ab361d39b1eb6d98cad962d2103
Andrew Hearin <ahearin@anl.gov>
2026-04-19 14:15:13 -0500

    Start skeleton implementation of _reproduce_mock_phot_kern

--- diffsky/experimental/dbk_phot_from_mock_merging.py
+++ diffsky/experimental/dbk_phot_from_mock_merging.py
@@ -2,9 +2,11 @@
 
 from jax import jit as jjit
 
+from .kernels import mc_randoms, phot_kernels, phot_kernels_merging
+
 
 @jjit
-def _reproduce_mock_dbk_merging_kern(
+def _reproduce_mock_phot_kern(
     mc_is_q,
     uran_av,
     uran_delta,
@@ -15,8 +17,6 @@ def _reproduce_mock_dbk_merging_kern(
     z_obs,
     t_obs,
     mah_params,
-    fknot,
-    uran_fbulge,
     ssp_data,
     precomputed_ssp_mag_table,
     z_phot_table,
@@ -25,7 +25,47 @@ def _reproduce_mock_dbk_merging_kern(
     spspop_params,
     scatter_params,
     ssp_err_pop_params,
+    merge_params,
     cosmo_params,
     fb,
+    logmp_infall,
+    logmhost_infall,
+    t_infall,
+    is_central,
+    nhalos_weights,
+    halo_indx,
 ):
-    pass
+    phot_randoms = mc_randoms.PhotRandoms(
+        mc_is_q,
+        uran_av,
+        uran_delta,
+        uran_funo,
+        uran_pburst,
+        delta_mag_ssp_scatter,
+    )
+
+    _res = phot_kernels_merging._phot_kern_merging(
+        phot_randoms,
+        sfh_params,
+        z_obs,
+        t_obs,
+        mah_params,
+        ssp_data,
+        precomputed_ssp_mag_table,
+        z_phot_table,
+        wave_eff_table,
+        mzr_params,
+        spspop_params,
+        scatter_params,
+        ssp_err_pop_params,
+        merge_params,
+        cosmo_params,
+        fb,
+        logmp_infall,
+        logmhost_infall,
+        t_infall,
+        is_central,
+        nhalos_weights,
+        halo_indx,
+    )
+    phot_kern_results, flux_obs, merge_prob, mstar_obs = _res
