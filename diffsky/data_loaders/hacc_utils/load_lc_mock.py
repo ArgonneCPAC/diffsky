f4724260cc95557d1f502893dd820a803aba8b0e
Andrew Hearin <ahearin@anl.gov>
2026-04-19 19:32:10 -0500

    sed_info is namedtuple not dict

--- diffsky/data_loaders/hacc_utils/load_lc_mock.py
+++ diffsky/data_loaders/hacc_utils/load_lc_mock.py
@@ -366,7 +366,7 @@ def compute_dbk_sed_from_diffsky_mock(
         sim_info.fb,
     )
     dbk_sed_info = dbk_phot_info._asdict()
-    dbk_sed_info["rest_sed_bulge"] = sed_info["rest_sed_bulge"]
-    dbk_sed_info["rest_sed_disk"] = sed_info["rest_sed_disk"]
-    dbk_sed_info["rest_sed_knots"] = sed_info["rest_sed_knots"]
+    dbk_sed_info["rest_sed_bulge"] = sed_info.rest_sed_bulge
+    dbk_sed_info["rest_sed_disk"] = sed_info.rest_sed_disk
+    dbk_sed_info["rest_sed_knots"] = sed_info.rest_sed_knots
     return dbk_sed_info
