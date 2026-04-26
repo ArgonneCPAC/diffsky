443502ee49fadadedb9a4d753ce7105bc91314d8
Andrew Hearin <ahearin@anl.gov>
2026-04-26 14:16:43 -0500

    Add cosmos_params_260316 calibration due to Kaustav

--- diffsky/param_utils/cosmos_calibrations/__init__.py
+++ diffsky/param_utils/cosmos_calibrations/__init__.py
@@ -5,6 +5,7 @@ from .cosmos_params_260120_UM import cosmos_260120_UM as _cosmos_260120_UM
 from .cosmos_params_260210 import cosmos_260210 as _cosmos_260210
 from .cosmos_params_260215 import cosmos_260215 as _cosmos_260215
 from .cosmos_params_260305_Cmrg import cosmos_260305_Cmrg as _cosmos_260305_Cmrg
+from .cosmos_params_260316 import cosmos_260316 as _cosmos_260316
 
 COSMOS_PARAM_FITS = dict()
 
@@ -15,3 +16,4 @@ COSMOS_PARAM_FITS["cosmos_260215"] = _cosmos_260215
 
 COSMOS_PARAM_FITS_MERGING = dict()
 COSMOS_PARAM_FITS_MERGING["cosmos_260305_Cmrg"] = _cosmos_260305_Cmrg
+COSMOS_PARAM_FITS_MERGING["cosmos_260316"] = _cosmos_260316
