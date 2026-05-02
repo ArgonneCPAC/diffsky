4c8aebc98558623c394513c94ba95c9e8790b8fa
Andrew Hearin <ahearin@anl.gov>
2026-05-02 10:53:44 -0500

    Remove compute_dbk_phot_from_diffsky_mock function

--- diffsky/data_loaders/hacc_utils/sed_from_mock.py
+++ diffsky/data_loaders/hacc_utils/sed_from_mock.py
@@ -79,64 +79,6 @@ def compute_phot_from_diffsky_mock_merging(lc_mock_chunk, metadata, tcurves=None
     return phot_kern_results, phot_randoms, merging_randoms
 
 
-def compute_dbk_phot_from_diffsky_mock(
-    *, diffsky_data, ssp_data, param_collection, sim_info, z_phot_table, tcurves
-):
-    mah_params = DEFAULT_MAH_PARAMS._make(
-        [diffsky_data[key] for key in DEFAULT_MAH_PARAMS._fields]
-    )
-    sfh_params = DEFAULT_DIFFSTAR_PARAMS._make(
-        [diffsky_data[key] for key in DEFAULT_DIFFSTAR_PARAMS._fields]
-    )
-    t_obs = age_at_z(diffsky_data["redshift_true"], *sim_info.cosmo_params)
-
-    wave_eff_table = phot_utils.get_wave_eff_table(z_phot_table, tcurves)
-
-    precomputed_ssp_mag_table = psspp.get_precompute_ssp_mag_redshift_table(
-        tcurves, ssp_data, z_phot_table, sim_info.cosmo_params
-    )
-
-    mc_is_q = jnp.where(diffsky_data["mc_sfh_type"] == 0, True, False)
-    args = (
-        mc_is_q,
-        diffsky_data["uran_av"],
-        diffsky_data["uran_delta"],
-        diffsky_data["uran_funo"],
-        diffsky_data["uran_pburst"],
-        diffsky_data["delta_mag_ssp_scatter"],
-        sfh_params,
-        diffsky_data["redshift_true"],
-        t_obs,
-        mah_params,
-        diffsky_data["fknot"],
-        diffsky_data["uran_fbulge"],
-        ssp_data,
-        precomputed_ssp_mag_table,
-        z_phot_table,
-        wave_eff_table,
-        param_collection.mzr_params,
-        param_collection.spspop_params,
-        param_collection.scatter_params,
-        param_collection.ssperr_params,
-        sim_info.cosmo_params,
-        sim_info.fb,
-    )
-    _res = dbk_phot_from_mock._reproduce_mock_dbk_kern(*args)
-    (
-        phot_info,
-        phot_randoms,
-        disk_bulge_history,
-        obs_mags_bulge,
-        obs_mags_disk,
-        obs_mags_knots,
-    ) = _res
-    phot_info = phot_info._asdict()
-    phot_info["obs_mags_bulge"] = obs_mags_bulge
-    phot_info["obs_mags_disk"] = obs_mags_disk
-    phot_info["obs_mags_knots"] = obs_mags_knots
-    return phot_info
-
-
 def compute_dbk_phot_from_diffsky_mock_merging(lc_mock_chunk, metadata, tcurves=None):
     if tcurves is None:
         tcurves = metadata["tcurves"]
