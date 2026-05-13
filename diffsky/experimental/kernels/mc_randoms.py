3ec74ee021dcafade09bf7522991198b0b4026d7
Andrew Hearin <ahearin@anl.gov>
2026-05-13 09:08:46 -0500

    Implement get_phot_randoms function

--- diffsky/experimental/kernels/mc_randoms.py
+++ diffsky/experimental/kernels/mc_randoms.py
@@ -26,6 +26,38 @@ DiffMergeRandoms = namedtuple("DiffMergeRandoms", ("uran_pmerge",))
 
 
 @jjit
+def get_phot_randoms(ran_key, diffstarpop_params, mah_params, cosmo_params):
+    n_gals = mah_params.logm0.size
+
+    # Monte Carlo diffstar params
+    ran_key, sfh_key = jran.split(ran_key, 2)
+    diffstarpop_results = mcdw.mc_diffstarpop_wrapper(
+        diffstarpop_params, sfh_key, mah_params, cosmo_params
+    )
+    # Generate randoms for stochasticity in dust attenuation curves
+    ran_key, dust_key = jran.split(ran_key, 2)
+    dust_randoms = sspwk.get_dust_randoms(dust_key, n_gals)
+
+    # Randoms for burstiness
+    ran_key, burst_key = jran.split(ran_key, 2)
+    uran_pburst = sspwk.get_burstiness_randoms(burst_key, n_gals)
+
+    # Scatter for SSP errors
+    ran_key, ssp_key = jran.split(ran_key, 2)
+    delta_mag_ssp_scatter = ssp_err_model.get_delta_mag_ssp_scatter(ssp_key, n_gals)
+
+    phot_randoms = PhotRandoms(
+        diffstarpop_results.mc_is_q,
+        dust_randoms.uran_av,
+        dust_randoms.uran_delta,
+        dust_randoms.uran_funo,
+        uran_pburst,
+        delta_mag_ssp_scatter,
+    )
+    return phot_randoms, diffstarpop_results
+
+
+@jjit
 def get_mc_phot_randoms(ran_key, diffstarpop_params, mah_params, cosmo_params):
     n_gals = mah_params.logm0.size
 
