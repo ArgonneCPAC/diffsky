a9b4b1440187a802fd45f1eb431eaba9d674f4ef
Andrew Hearin <ahearin@anl.gov>
2026-05-30 12:12:09 -0500

    Add gd_mc_phot_kernels.py

---
+++ diffsky/experimental/kernels/gd_mc_phot_kernels.py
@@ -0,0 +1,50 @@
+""""""
+
+from . import (
+    constants,
+    dbk_kernels,
+    dbk_specphot_kernels,
+    gd_linelum_kernels,
+    gd_phot_kernels,
+    gd_phot_kernels_merging,
+    gd_specphot_kernels_merging,
+    mc_randoms,
+    sed_kernels,
+)
+
+# constants
+LGMET_SCATTER = constants.LGMET_SCATTER
+
+
+# kernels
+_mc_phot_kern = gd_phot_kernels._mc_phot_kern
+_phot_kern = gd_phot_kernels._phot_kern
+_mc_dbk_kern = dbk_kernels._mc_dbk_kern
+_dbk_kern = dbk_kernels._dbk_kern  # noqa
+_mc_specphot_kern = gd_linelum_kernels._mc_specphot_kern
+_specphot_kern = gd_linelum_kernels._specphot_kern
+_get_dbk_phot_from_dbk_weights = dbk_kernels._get_dbk_phot_from_dbk_weights
+_sed_kern = sed_kernels._sed_kern
+_get_dbk_linelum_decomposition = dbk_kernels._get_dbk_linelum_decomposition
+_mc_phot_kern_merging = gd_phot_kernels_merging._mc_phot_kern_merging
+_phot_kern_merging = gd_phot_kernels_merging._phot_kern_merging
+_mc_specphot_kern_merging = gd_specphot_kernels_merging._mc_specphot_kern_merging
+_specphot_kern_merging = gd_specphot_kernels_merging._specphot_kern_merging
+
+_mc_dbk_phot_kern = dbk_specphot_kernels._mc_dbk_phot_kern
+_mc_dbk_specphot_kern = dbk_specphot_kernels._mc_dbk_specphot_kern
+interp_vmap2 = gd_phot_kernels.interp_vmap2
+
+
+# randoms
+get_mc_phot_randoms = mc_randoms.get_mc_phot_randoms
+get_mc_dbk_randoms = mc_randoms.get_mc_dbk_randoms
+
+# namedtuple containers
+PhotRandoms = mc_randoms.PhotRandoms
+SpecKernResults = gd_linelum_kernels.SpecKernResults
+PhotKernResults = gd_phot_kernels.PhotKernResults
+DBKRandoms = mc_randoms.DBKRandoms
+SEDKernResults = sed_kernels.SEDKernResults
+SEDKernResults = sed_kernels.SEDKernResults
+MCDBKPhotInfo = dbk_specphot_kernels.MCDBKPhotInfo
