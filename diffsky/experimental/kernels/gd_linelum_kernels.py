6dc178d751f3adef1c483fa9bca9323298312612
Andrew Hearin <ahearin@anl.gov>
2026-05-30 11:18:19 -0500

    Add linelum_weighted to SpecKernResults

--- diffsky/experimental/kernels/gd_linelum_kernels.py
+++ diffsky/experimental/kernels/gd_linelum_kernels.py
@@ -111,7 +111,7 @@ def _specphot_kern(
         fb,
     )
 
-    _dust_res = sspwk.compute_dust_attenuation_lines(
+    _dust_res_mc = sspwk.compute_dust_attenuation_lines(
         phot_randoms.uran_av,
         phot_randoms.uran_delta,
         phot_randoms.uran_funo,
@@ -123,18 +123,96 @@ def _specphot_kern(
         spspop_params.dustpop_params,
         scatter_params,
     )
-    dust_ftrans_lines = _dust_res[0]
+    dust_ftrans_lines_mc = _dust_res_mc[0]
 
     linelum_gal = sspwk._compute_linelum_from_weights(
         phot_kern_results.logsm_obs,
-        dust_ftrans_lines,
+        dust_ftrans_lines_mc,
         ssp_data,
         phot_kern_results.ssp_weights,
     )
 
-    spec_kern_results = SpecKernResults(linelum_gal, dust_ftrans_lines)
+    _dust_res_q = sspwk.compute_dust_attenuation_lines(
+        phot_randoms.uran_av,
+        phot_randoms.uran_delta,
+        phot_randoms.uran_funo,
+        phot_kern_results.diffstar_info_q.logsm_obs,
+        phot_kern_results.diffstar_info_q.logssfr_obs,
+        ssp_data,
+        z_obs,
+        line_wave_table,
+        spspop_params.dustpop_params,
+        scatter_params,
+    )
+    dust_ftrans_lines_q = _dust_res_q[0]
+
+    linelum_q = sspwk._compute_linelum_from_weights(
+        phot_kern_results.diffstar_info_q.logsm_obs,
+        dust_ftrans_lines_q,
+        ssp_data,
+        phot_kern_results.burstiness_info_q.ssp_weights_smooth,
+    )
+
+    _dust_res_ms = sspwk.compute_dust_attenuation_lines(
+        phot_randoms.uran_av,
+        phot_randoms.uran_delta,
+        phot_randoms.uran_funo,
+        phot_kern_results.diffstar_info_ms.logsm_obs,
+        phot_kern_results.diffstar_info_ms.logssfr_obs,
+        ssp_data,
+        z_obs,
+        line_wave_table,
+        spspop_params.dustpop_params,
+        scatter_params,
+    )
+    dust_ftrans_lines_ms = _dust_res_ms[0]
+
+    linelum_ms = sspwk._compute_linelum_from_weights(
+        phot_kern_results.diffstar_info_ms.logsm_obs,
+        dust_ftrans_lines_ms,
+        ssp_data,
+        phot_kern_results.burstiness_info_ms.ssp_weights_smooth,
+    )
+
+    linelum_bursty = sspwk._compute_linelum_from_weights(
+        phot_kern_results.diffstar_info_ms.logsm_obs,
+        dust_ftrans_lines_ms,
+        ssp_data,
+        phot_kern_results.burstiness_info_ms.ssp_weights_bursty,
+    )
+
+    n_gals = linelum_q.shape[0]
+    fq = diffstarpop_results.frac_q
+    weights_q = fq
+    weights_ms = (1 - fq) * (1 - phot_kern_results.burstiness_info_ms.p_burst)
+    weights_bursty = (1 - fq) * phot_kern_results.burstiness_info_ms.p_burst
+
+    linelum_weighted = (
+        (weights_q.reshape((n_gals, 1)) * linelum_q)
+        + (weights_ms.reshape((n_gals, 1)) * linelum_ms)
+        + (weights_bursty.reshape((n_gals, 1)) * linelum_bursty)
+    )
+
+    spec_kern_results = SpecKernResults(
+        linelum_gal,
+        linelum_weighted,
+        dust_ftrans_lines_mc,
+        linelum_ms,
+        linelum_q,
+        linelum_bursty,
+    )
 
     return phot_kern_results, spec_kern_results
 
 
-SpecKernResults = namedtuple("SpecKernResults", ("linelum_gal", "dust_ftrans_lines"))
+SpecKernResults = namedtuple(
+    "SpecKernResults",
+    (
+        "linelum_gal",
+        "linelum_weighted",
+        "dust_ftrans_lines",
+        "linelum_ms",
+        "linelum_q",
+        "linelum_bursty",
+    ),
+)
