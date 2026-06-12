b15aca6b4157dbf4648b3a616da2e73d61e56cec
Andrew Hearin <ahearin@anl.gov>
2026-06-12 11:33:42 -0500

    Starting to build gd_sed_from_mock.py

--- diffsky/experimental/kernels/sed_kernels_merging.py
+++ diffsky/experimental/kernels/sed_kernels_merging.py
@@ -1,15 +1,14 @@
 """"""
 
 from collections import namedtuple
-from . import sed_kernels
 from functools import partial
 
 from jax import jit as jjit
 from jax import numpy as jnp
 
-
+from ...merging import merging_kernels, merging_model
 from .. import mc_diffstarpop_wrappers as mcdw
-from ...merging import merging_model, merging_kernels
+from . import sed_kernels
 
 
 @partial(jjit, static_argnames=["n_t_table"])
