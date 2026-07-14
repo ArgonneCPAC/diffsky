ed81d7c68df5f72f0ffd6f9161fa5523681135f6
nvill <natvnr@gmail.com>
2026-07-14 04:04:24 -0500

    Organize existing lightcone phot code into new modules.

---
+++ diffsky/experimental/lightcone_generators/lc_phot.py
@@ -0,0 +1,355 @@
+# flake8: noqa: E402
+
+# diffsky/lightcone_generators/lc_phot.py
+"""
+The functions in this module receive as input LCPhotData and
+diffsky parameters (ParamCollection), and populate the halo lightcone with
+galaxy photometry.
+
+LCPhotData is computed in ./lc_phot_data.py.
+"""
+
+from jax import config
+
+config.update("jax_enable_x64", True)
+
+from collections import namedtuple
+
+from diffstar import DEFAULT_DIFFSTAR_PARAMS
+from diffstar.defaults import FB
+from dsps.cosmology import DEFAULT_COSMOLOGY
+from jax import jit as jjit
+
+from ...param_utils import diffsky_param_wrapper_merging as dpwm
+from ..kernels import (
+    _dbk_sed_kern,
+    _mc_dbk_photline_kern_merging,
+    _mc_phot_kern_merging,
+    _mc_photline_kern_merging,
+    _sed_kern,
+    mc_randoms,
+)
+
+
+@jjit
+def mc_lc_phot(
+    ran_key,
+    lc_data,
+    mc_merge,
+    *,
+    param_collection=dpwm.DEFAULT_PARAM_COLLECTION,
+    cosmo_params=DEFAULT_COSMOLOGY,
+    fb=FB,
+):
+    """Populate the input lightcone with galaxy photometry
+
+    Parameters
+    ----------
+    ran_key : jax.random.key
+
+    lc_data : namedtuple
+        Contains info about the halo lightcone, SED inputs, and diffsky parameters
+
+    mc_merge : int
+        Integer specifying whether merging is stochastic.
+        Use mc_merge=0 in gradient descent applications, and mc_merge=1 for mock-making
+
+    Returns
+    -------
+    phot_kern_results : namedtuple
+        Contains info about the galaxy SEDs
+
+    phot_randoms : namedtuple
+        Contains info about randoms used in photometry
+
+    merging_randoms : namedtuple
+        Contains info about randoms used in merging
+
+    """
+
+    args = (
+        ran_key,
+        lc_data.z_obs,
+        lc_data.t_obs,
+        lc_data.mah_params,
+        lc_data.ssp_data,
+        lc_data.precomputed_ssp_mag_table,
+        lc_data.z_phot_table,
+        lc_data.wave_eff_table,
+        param_collection.diffstarpop_params,
+        param_collection.mzr_params,
+        param_collection.spspop_params,
+        param_collection.scatter_params,
+        param_collection.ssperr_params,
+        param_collection.merging_params,
+        cosmo_params,
+        fb,
+        lc_data.logmp_infall,
+        lc_data.logmhost_infall,
+        lc_data.t_infall,
+        lc_data.is_central,
+        lc_data.sat_weight,
+        lc_data.halo_indx,
+        mc_merge,
+    )
+
+    phot_kern_results, phot_randoms, merging_randoms = _mc_phot_kern_merging(*args)
+    return phot_kern_results, phot_randoms, merging_randoms
+
+
+@jjit
+def mc_lc_photline(
+    ran_key,
+    lc_data,
+    mc_merge,
+    *,
+    param_collection=dpwm.DEFAULT_PARAM_COLLECTION,
+    cosmo_params=DEFAULT_COSMOLOGY,
+    fb=FB,
+):
+    """Populate the input lightcone with galaxy photometry
+
+    Parameters
+    ----------
+    ran_key : jax.random.key
+
+    lc_data : namedtuple
+        Contains info about the halo lightcone, SED inputs, and diffsky parameters
+
+    Returns
+    -------
+    results : dict
+        Contains info about the galaxy SEDs
+
+    """
+    args = (
+        ran_key,
+        lc_data.z_obs,
+        lc_data.t_obs,
+        lc_data.mah_params,
+        lc_data.ssp_data,
+        lc_data.precomputed_ssp_mag_table,
+        lc_data.z_phot_table,
+        lc_data.wave_eff_table,
+        lc_data.line_wave_table,
+        param_collection.diffstarpop_params,
+        param_collection.mzr_params,
+        param_collection.spspop_params,
+        param_collection.scatter_params,
+        param_collection.ssperr_params,
+        param_collection.merging_params,
+        cosmo_params,
+        fb,
+        lc_data.logmp_infall,
+        lc_data.logmhost_infall,
+        lc_data.t_infall,
+        lc_data.is_central,
+        lc_data.sat_weight,
+        lc_data.halo_indx,
+        mc_merge,
+    )
+
+    _res = _mc_photline_kern_merging(*args)
+    phot_kern_results, phot_randoms, spec_kern_results = _res
+
+    line_fields = list(lc_data.ssp_data.ssp_emline_wave._fields)
+    fields = list(phot_kern_results._fields) + line_fields
+    n_lines = len(line_fields)
+    lines = [spec_kern_results.linelum_gal[:, i] for i in range(n_lines)]
+
+    SpecPhotResults = namedtuple("SpecPhotResults", fields)
+    spec_phot_results = SpecPhotResults(*phot_kern_results, *lines)
+
+    return spec_phot_results
+
+
+@jjit
+def mc_lc_sed(
+    ran_key,
+    lc_data,
+    mc_merge,
+    *,
+    param_collection=dpwm.DEFAULT_PARAM_COLLECTION,
+    cosmo_params=DEFAULT_COSMOLOGY,
+    fb=FB,
+):
+    """Populate the input lightcone with galaxy SEDs
+
+    Parameters
+    ----------
+    ran_key : jax.random.key
+
+    lc_data : namedtuple
+        Contains info about the halo lightcone, SED inputs, and diffsky parameters
+
+    Returns
+    -------
+    results : dict
+        Contains info about the galaxy SEDs
+
+    """
+
+    phot_kern_results, phot_randoms, merging_randoms = _mc_phot_kern_merging(
+        ran_key,
+        lc_data.z_obs,
+        lc_data.t_obs,
+        lc_data.mah_params,
+        lc_data.ssp_data,
+        lc_data.precomputed_ssp_mag_table,
+        lc_data.z_phot_table,
+        lc_data.wave_eff_table,
+        *param_collection,
+        DEFAULT_COSMOLOGY,
+        fb,
+        lc_data.logmp_infall,
+        lc_data.logmhost_infall,
+        lc_data.t_infall,
+        lc_data.is_central,
+        lc_data.sat_weight,
+        lc_data.halo_indx,
+        mc_merge,
+    )
+    sfh_params = DEFAULT_DIFFSTAR_PARAMS._make(
+        [getattr(phot_kern_results, key) for key in DEFAULT_DIFFSTAR_PARAMS._fields]
+    )
+
+    args = (
+        phot_randoms,
+        merging_randoms,
+        sfh_params,
+        lc_data.z_obs,
+        lc_data.t_obs,
+        lc_data.mah_params,
+        lc_data.ssp_data,
+        param_collection.mzr_params,
+        param_collection.spspop_params,
+        param_collection.scatter_params,
+        param_collection.ssperr_params,
+        param_collection.merging_params,
+        cosmo_params,
+        fb,
+        lc_data.logmp_infall,
+        lc_data.logmhost_infall,
+        lc_data.t_infall,
+        lc_data.is_central,
+        lc_data.sat_weight,
+        lc_data.halo_indx,
+        mc_merge,
+    )
+    sed_kern_results = _sed_kern(*args)
+    return sed_kern_results
+
+
+@jjit
+def mc_lc_dbk_photline(
+    ran_key,
+    lc_data,
+    mc_merge,
+    *,
+    param_collection=dpwm.DEFAULT_PARAM_COLLECTION,
+    cosmo_params=DEFAULT_COSMOLOGY,
+    fb=FB,
+):
+    """Populate the input lightcone with disk/bulge/knot photometry
+
+    Parameters
+    ----------
+    ran_key : jax.random.key
+
+    lc_data : namedtuple
+        Contains info about the halo lightcone, SED inputs, and diffsky parameters
+
+    Returns
+    -------
+    results : dict
+        Contains info about disk/bulge/knot photometry
+
+    """
+
+    args = (
+        ran_key,
+        lc_data.z_obs,
+        lc_data.t_obs,
+        lc_data.mah_params,
+        lc_data.ssp_data,
+        lc_data.precomputed_ssp_mag_table,
+        lc_data.z_phot_table,
+        lc_data.wave_eff_table,
+        lc_data.line_wave_table,
+        param_collection.diffstarpop_params,
+        param_collection.mzr_params,
+        param_collection.spspop_params,
+        param_collection.scatter_params,
+        param_collection.ssperr_params,
+        param_collection.merging_params,
+        cosmo_params,
+        fb,
+        lc_data.logmp_infall,
+        lc_data.logmhost_infall,
+        lc_data.t_infall,
+        lc_data.is_central,
+        lc_data.sat_weight,
+        lc_data.halo_indx,
+        mc_merge,
+    )
+    dbk_phot_info, dbk_weights = _mc_dbk_photline_kern_merging(*args)
+    return dbk_phot_info
+
+
+@jjit
+def mc_lc_dbk_sed(
+    ran_key,
+    lc_data,
+    mc_merge,
+    *,
+    param_collection=dpwm.DEFAULT_PARAM_COLLECTION,
+    cosmo_params=DEFAULT_COSMOLOGY,
+    fb=FB,
+):
+    """Populate the input lightcone with disk/bulge/knot SEDs
+
+    Parameters
+    ----------
+    ran_key : jax.random.key
+
+    lc_data : namedtuple
+        Contains info about the halo lightcone, SED inputs, and diffsky parameters
+
+    Returns
+    -------
+    results : dict
+        Contains info about disk/bulge/knot SEDs
+
+    """
+
+    _res = mc_randoms.get_mc_dbk_phot_merge_randoms(
+        ran_key, param_collection.diffstarpop_params, lc_data.mah_params, cosmo_params
+    )
+    phot_randoms, sfh_params, dbk_randoms, merging_randoms = _res
+
+    args = (
+        phot_randoms,
+        dbk_randoms,
+        merging_randoms,
+        sfh_params,
+        lc_data.z_obs,
+        lc_data.t_obs,
+        lc_data.mah_params,
+        lc_data.ssp_data,
+        param_collection.mzr_params,
+        param_collection.spspop_params,
+        param_collection.scatter_params,
+        param_collection.ssperr_params,
+        param_collection.merging_params,
+        cosmo_params,
+        fb,
+        lc_data.logmp_infall,
+        lc_data.logmhost_infall,
+        lc_data.t_infall,
+        lc_data.is_central,
+        lc_data.sat_weight,
+        lc_data.halo_indx,
+        mc_merge,
+    )
+    dbk_sed_info = _dbk_sed_kern(*args)
+    return dbk_sed_info
---
+++ diffsky/experimental/lightcone_generators/tests/test_lc_phot_data.py
@@ -0,0 +1,173 @@
+""""""
+
+from collections import namedtuple
+
+import numpy as np
+from dsps.data_loaders import load_emline_info as lemi
+from dsps.data_loaders import retrieve_fake_fsps_data
+from dsps.data_loaders.defaults import TransmissionCurve
+from jax import random as jran
+
+from ....data_loaders import load_ssp_data
+from .. import lc_phot_data
+from .. import lc_phot
+from . import test_lc_phot as tlcp
+
+
+def _get_weighted_lc_photdata_for_unit_testing(
+    num_halos=75, n_lines=3, z_min=0.1, z_max=3.0, n_z_phot_table=15, ssp_data=None
+):
+    ran_key = jran.key(0)
+
+    lgmp_min, lgmp_max = 10.0, 15.0
+    sky_area_degsq = 100.0
+
+    if ssp_data is None:
+        ssp_data = load_ssp_data.load_fake_ssp_data()
+
+    _res = retrieve_fake_fsps_data.load_fake_filter_transmission_curves()
+    wave, u, g, r, i, z, y = _res
+
+    tcurve_list = [TransmissionCurve(wave, x) for x in (u, i, y)]
+    names = [f"lsst_{x}" for x in ("u", "i", "y")]
+    TransmissionCurves = namedtuple("TransmissionCurves", names)
+    tcurves = TransmissionCurves(*tcurve_list)
+
+    z_phot_table = 10 ** np.linspace(np.log10(z_min), np.log10(z_max), n_z_phot_table)
+
+    args = (
+        ran_key,
+        num_halos,
+        z_min,
+        z_max,
+        lgmp_min,
+        lgmp_max,
+        sky_area_degsq,
+        ssp_data,
+        tcurves,
+        z_phot_table,
+    )
+    lc_data = lc_phot_data.weighted_lc_photdata(*args)
+
+    emline_names = lc_data.ssp_data.ssp_emline_wave._fields[0:n_lines]
+    ssp_data = lemi.get_subset_emline_data(lc_data.ssp_data, emline_names)
+    lc_data = lc_data._replace(
+        ssp_data=ssp_data,
+        line_wave_table=lc_data.line_wave_table[0:n_lines],
+        precomputed_ssp_linelum_cgs_table=lc_data.precomputed_ssp_linelum_cgs_table[
+            :n_lines, :, :
+        ],
+    )
+
+    return lc_data, tcurves
+
+
+def test_weighted_lc_photdata():
+    num_halos = 75
+    lc_data, tcurves = _get_weighted_lc_photdata_for_unit_testing(num_halos=num_halos)
+    n_tot = lc_data.z_obs.size
+    shape_ntot_keys = (
+        "z_obs",
+        "t_obs",
+        "logmp_obs",
+        "logmp0",
+        "t_infall",
+        "logmp_infall",
+        "logmhost_infall",
+        "is_central",
+        "halo_indx",
+    )
+    for lc_key in shape_ntot_keys:
+        arr = getattr(lc_data, lc_key)
+        assert arr.shape == (n_tot,), f"lc_data.{lc_key} has the wrong shape"
+        assert np.all(np.isfinite(arr)), f"lc_data.{lc_key} has NaNs"
+
+    for field in lc_phot_data.LCPhotData._fields:
+        assert hasattr(lc_data, field)
+
+    for arr in lc_data.mah_params:
+        assert np.all(np.isfinite(arr))
+
+    ran_key = jran.key(1)
+    mc_merge = 0
+    phot_kern_results = lc_phot.mc_lc_phot(ran_key, lc_data, mc_merge)[0]
+    tlcp.check_phot_kern_results(phot_kern_results)
+
+
+def _get_lc_photdata_for_unit_testing(
+    n_lines=3, z_min=0.1, z_max=0.3, n_z_phot_table=15, ssp_data=None
+):
+    ran_key = jran.key(0)
+
+    lgmp_min = 13.0
+    lgmsub_min = 13.0
+    sky_area_degsq = 1.0
+
+    if ssp_data is None:
+        ssp_data = load_ssp_data.load_fake_ssp_data()
+
+    _res = retrieve_fake_fsps_data.load_fake_filter_transmission_curves()
+    wave, u, g, r, i, z, y = _res
+
+    tcurve_list = [TransmissionCurve(wave, x) for x in (u, i, y)]
+    names = [f"lsst_{x}" for x in ("u", "i", "y")]
+    TransmissionCurves = namedtuple("TransmissionCurves", names)
+    tcurves = TransmissionCurves(*tcurve_list)
+
+    z_phot_table = 10 ** np.linspace(np.log10(z_min), np.log10(z_max), n_z_phot_table)
+
+    args = (
+        ran_key,
+        z_min,
+        z_max,
+        lgmp_min,
+        lgmsub_min,
+        sky_area_degsq,
+        ssp_data,
+        tcurves,
+        z_phot_table,
+    )
+    lc_data = lc_phot_data.mc_lc_photdata(*args)
+
+    emline_names = lc_data.ssp_data.ssp_emline_wave._fields[0:n_lines]
+    ssp_data = lemi.get_subset_emline_data(lc_data.ssp_data, emline_names)
+    lc_data = lc_data._replace(
+        ssp_data=ssp_data,
+        line_wave_table=lc_data.line_wave_table[0:n_lines],
+        precomputed_ssp_linelum_cgs_table=lc_data.precomputed_ssp_linelum_cgs_table[
+            :n_lines, :, :
+        ],
+    )
+
+    return lc_data, tcurves
+
+
+def test_lc_photdata():
+    lc_data, tcurves = _get_lc_photdata_for_unit_testing()
+    n_tot = lc_data.z_obs.size
+    shape_ntot_keys = (
+        "z_obs",
+        "t_obs",
+        "logmp_obs",
+        "logmp0",
+        "t_infall",
+        "logmp_infall",
+        "logmhost_infall",
+        "is_central",
+        "halo_indx",
+    )
+    for lc_key in shape_ntot_keys:
+        arr = getattr(lc_data, lc_key)
+        assert arr.shape == (n_tot,), f"lc_data.{lc_key} has the wrong shape"
+        assert np.all(np.isfinite(arr)), f"lc_data.{lc_key} has NaNs"
+
+    for field in lc_phot_data.LCPhotData._fields:
+        assert hasattr(lc_data, field)
+
+    for arr in lc_data.mah_params:
+        assert np.all(np.isfinite(arr))
+
+    ran_key = jran.key(1)
+    mc_merge = 0
+    phot_kern_results = lc_phot.mc_lc_phot(ran_key, lc_data, mc_merge)[0]
+    tlcp.check_phot_kern_results(phot_kern_results)
