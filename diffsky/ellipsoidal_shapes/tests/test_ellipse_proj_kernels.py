db3e18d3353b92c5fbba589b7cf5089b6da1d829
Andrew Hearin <ahearin@anl.gov>
2026-09-08 07:17:35 -0500

    Add failing test of compute_ellipse2d function

--- diffsky/ellipsoidal_shapes/tests/test_ellipse_proj_kernels.py
+++ diffsky/ellipsoidal_shapes/tests/test_ellipse_proj_kernels.py
@@ -88,6 +88,22 @@ def test_projected_beta_alpha_respect_mathematical_bounds():
     assert np.all(ellipse2d.beta <= ellipse2d.alpha)
     assert np.any(ellipse2d.beta < ellipse2d.alpha)
 
+    # Enforce 2d axis ratio is bounded by the min/max of 3d axis ratios
+    axis_ratio_2d = ellipse2d.beta / ellipse2d.alpha
+    ba_axis_ratio_3d = b / a
+    ca_axis_ratio_3d = c / a
+    cb_axis_ratio_3d = c / b
+
+    axis_ratio_2d_min = np.minimum(
+        np.minimum(ba_axis_ratio_3d, ca_axis_ratio_3d), cb_axis_ratio_3d
+    )
+    axis_ratio_2d_max = np.maximum(
+        np.maximum(ba_axis_ratio_3d, ca_axis_ratio_3d), cb_axis_ratio_3d
+    )
+
+    assert np.all(axis_ratio_2d >= axis_ratio_2d_min)
+    assert np.all(axis_ratio_2d <= axis_ratio_2d_max)
+
 
 def test_calculate_ellipse2d_psi_respect_mathematical_bounds():
     """Enforce angle psi in the uv-plane respects mathematical bounds"""
