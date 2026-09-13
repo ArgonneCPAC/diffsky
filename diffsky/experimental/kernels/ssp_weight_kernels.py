9aa2f6b2635c8af3085a501e65b9ba42e75d32e5
Andrew Hearin <ahearin@anl.gov>
2026-09-13 14:24:37 -0500

    phot_kernels_in_situ._mc_phot_kern now only uses einsum implementation

--- diffsky/experimental/kernels/ssp_weight_kernels.py
+++ diffsky/experimental/kernels/ssp_weight_kernels.py
@@ -320,38 +320,7 @@ def _compute_obs_mags_from_weights(
 
 
 @jjit
-def _compute_obs_mags_from_weights_einsum(
-    logsm_obs, frac_trans, frac_ssp_err, ssp_photflux_table, ssp_weights
-):
-    photflux_galpop = _compute_obs_flux_from_weights_einsum(
-        logsm_obs, frac_trans, frac_ssp_err, ssp_photflux_table, ssp_weights
-    )
-    obs_mags = -2.5 * jnp.log10(photflux_galpop)
-    return obs_mags
-
-
-@jjit
 def _compute_obs_flux_from_weights(
-    logsm_obs, frac_trans, frac_ssp_err, ssp_photflux_table, ssp_weights
-):
-    n_gals = logsm_obs.size
-    n_bands, n_met, n_age = ssp_photflux_table.shape[1:]
-
-    # Reshape arrays before calculating galaxy magnitudes
-    _ferr_ssp = frac_ssp_err.reshape((n_gals, n_bands, 1, 1))
-    _ftrans = frac_trans.reshape((n_gals, n_bands, 1, n_age))
-    _weights = ssp_weights.reshape((n_gals, 1, n_met, n_age))
-    _mstar = 10 ** logsm_obs.reshape((n_gals, 1))
-
-    # Calculate galaxy magnitudes as PDF-weighted sums
-    integrand = ssp_photflux_table * _weights * _ftrans * _ferr_ssp
-    photflux_galpop = jnp.sum(integrand, axis=(2, 3)) * _mstar
-
-    return photflux_galpop
-
-
-@jjit
-def _compute_obs_flux_from_weights_einsum(
     logsm_obs, frac_trans, frac_ssp_err, ssp_photflux_table, ssp_weights
 ):
     n_gals = logsm_obs.size
