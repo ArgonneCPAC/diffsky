0fbe649e547a5132d896064b8b32433006887525
Ben Levine <60677145+bclevine@users.noreply.github.com>
2026-09-14 15:20:14 -0500

    Corrected some typos in shape metadata

--- diffsky/data_loaders/hacc_utils/metadata_mock.py
+++ diffsky/data_loaders/hacc_utils/metadata_mock.py
@@ -400,15 +400,15 @@ def add_metadata_dbk_morphology_columns(metadata):
 
     metadata["beta_disk"] = (
         str(u.kpc),
-        "2d projected size of the semi-major axis of the disk",
+        "2d projected size of the semi-minor axis of the disk",
     )
     metadata["alpha_disk"] = (
         str(u.kpc),
-        "2d projected size of the semi-minor axis of the disk",
+        "2d projected size of the semi-major axis of the disk",
     )
     metadata["ellipticity_disk"] = (
         str(u.dimensionless_unscaled),
-        "2d projected size of the semi-major axis of the disk",
+        "2d projected ellipticity of the disk",
     )
     metadata["psi_disk"] = (
         str(u.dimensionless_unscaled),
@@ -417,15 +417,15 @@ def add_metadata_dbk_morphology_columns(metadata):
 
     metadata["beta_bulge"] = (
         str(u.kpc),
-        "2d projected size of the semi-major axis of the bulge",
+        "2d projected size of the semi-minor axis of the bulge",
     )
     metadata["alpha_bulge"] = (
         str(u.kpc),
-        "2d projected size of the semi-minor axis of the bulge",
+        "2d projected size of the semi-major axis of the bulge",
     )
     metadata["ellipticity_bulge"] = (
         str(u.dimensionless_unscaled),
-        "2d projected size of the semi-major axis of the bulge",
+        "2d projected ellipticity of the bulge",
     )
     metadata["psi_bulge"] = (
         str(u.dimensionless_unscaled),
@@ -531,7 +531,6 @@ def append_metadata(
         column_metadata.pop(colname)
 
     with h5py.File(fnout, "r+") as hdf_out:
-
         metadata_group = hdf_out.require_group("metadata")
 
         metadata_group["z_phot_table"] = z_phot_table
@@ -580,7 +579,6 @@ def append_metadata(
 
         # Filter magnitudes
         for nickname in filter_nicknames:
-
             # Composite magnitudes
             key_out = "data/" + nickname
             assert key_out in hdf_out.keys(), f"{key_out} is missing from {fnout}"
@@ -598,9 +596,9 @@ def append_metadata(
             else:
                 for component in ("bulge", "disk", "knots"):
                     key_out = "data/" + "_".join((nickname, component))
-                    assert (
-                        key_out in hdf_out.keys()
-                    ), f"{key_out} is missing from {fnout}"
+                    assert key_out in hdf_out.keys(), (
+                        f"{key_out} is missing from {fnout}"
+                    )
 
                     msg = COMPONENT_MAG_MSG_PAT.format(component)
                     hdf_out[key_out].attrs["unit"] = str(u.ABmag)
@@ -613,7 +611,6 @@ def append_metadata(
 
         # emission line fluxes
         for linename in lineflux_nicknames:
-
             key_out = "data/" + linename
             assert key_out in hdf_out.keys(), f"{key_out} is missing from {fnout}"
 
